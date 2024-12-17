8d1f2cc096948ca8eb6cd6ad0551408497ac1297
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-17 10:26:26 -0300

    test(auth): enhance AuthController integration tests with additional scenarios and setup
    - **AuthControllerTestIT.java**:
      - Add `createTestingUser` method to set up a test user in the database before all tests.
      - Add `testLoginWithValidRequestBodyAndValidUser` to verify successful login with valid credentials.
      - Add `testLoginWithValidRequestBodyAndInvalidUser` to verify login failure with invalid credentials.
      - Update `testLoginWithValidRequestBody` to expect `NotFound` status for non-existent user.
    
    These changes improve the coverage and robustness of the integration tests for the authentication controller.

--- server/src/test/java/com/barataribeiro/sentinelofliberty/controllers/AuthControllerTestIT.java
+++ server/src/test/java/com/barataribeiro/sentinelofliberty/controllers/AuthControllerTestIT.java
@@ -1,11 +1,17 @@
 package com.barataribeiro.sentinelofliberty.controllers;
 
+import com.barataribeiro.sentinelofliberty.models.entities.User;
+import com.barataribeiro.sentinelofliberty.models.enums.Roles;
+import com.barataribeiro.sentinelofliberty.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +28,43 @@ class AuthControllerTestIT {
     private static final String BASE_URL = "/api/v1/auth";
     private final MockMvc mockMvc;
 
+    @BeforeAll
+    public static void createTestingUser(@Autowired @NotNull PasswordEncoder passwordEncoder,
+                                         @Autowired @NotNull UserRepository userRepository) {
+        User user = User.builder()
+                        .username("testuser")
+                        .email("testuser@example.com")
+                        .password(passwordEncoder.encode("testpassword"))
+                        .role(Roles.USER)
+                        .displayName("Test User")
+                        .build();
+        userRepository.save(user);
+    }
+
+    @Test
+    void testLoginWithValidRequestBodyAndValidUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/login")
+                                              .contentType("application/json")
+                                              .content(
+                                                      "{\"username\": \"testuser\", \"password\": \"testpassword\", " +
+                                                              "\"rememberMe\": true}"))
+               .andExpect(MockMvcResultMatchers.status().isOk())
+               .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    void testLoginWithValidRequestBodyAndInvalidUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/login")
+                                              .contentType("application/json")
+                                              .content(
+                                                      "{\"username\": \"testuser\", \"password\": \"wrongpassword\", " +
+                                                              "\"rememberMe\": true}"))
+               .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+               .andDo(MockMvcResultHandlers.print())
+               .andExpect(MockMvcResultMatchers.jsonPath("$.detail")
+                                               .value("Login failed; Wrong username or password."));
+    }
+
     @Test
     void testLoginWithValidRequestBody() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/login")
@@ -30,7 +73,7 @@ class AuthControllerTestIT {
                                                       "{\"username\": \"test\", \"password\": \"test\", " +
                                                               "\"rememberMe\": true}"))
                .andDo(MockMvcResultHandlers.print())
-               .andExpect(MockMvcResultMatchers.status().isOk());
+               .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
