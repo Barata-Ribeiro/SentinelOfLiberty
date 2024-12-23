0ad9d549dfd0bbd39379bb621e6801d670a87660
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-23 10:28:31 -0300

    test(application): enhance ServerApplicationTests with additional context load assertions
    - **ServerApplicationTests.java**:
      - Add imports for `ArticleController`, `AuthController`, `ArticleService`, and `AuthService`.
      - Add `@RequiredArgsConstructor` annotation to inject dependencies.
      - Add fields for `serverApplication`, `authController`, `authService`, `articleController`, and `articleService`.
      - Enhance `contextLoads` test to assert that all injected components are not null.
    
    These changes improve the test coverage by ensuring that all critical components of the application context are loaded correctly.

--- server/src/test/java/com/barataribeiro/sentinelofliberty/ServerApplicationTests.java
+++ server/src/test/java/com/barataribeiro/sentinelofliberty/ServerApplicationTests.java
@@ -1,18 +1,37 @@
 package com.barataribeiro.sentinelofliberty;
 
+import com.barataribeiro.sentinelofliberty.controllers.ArticleController;
+import com.barataribeiro.sentinelofliberty.controllers.AuthController;
+import com.barataribeiro.sentinelofliberty.services.ArticleService;
+import com.barataribeiro.sentinelofliberty.services.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 class ServerApplicationTests {
+    private final ServerApplication serverApplication;
+    private final AuthController authController;
+    private final AuthService authService;
+    private final ArticleController articleController;
+    private final ArticleService articleService;
 
     @Test
     void contextLoads() {
+        assertThat(serverApplication).isNotNull();
+        assertThat(authController).isNotNull();
+        assertThat(authService).isNotNull();
+        assertThat(articleController).isNotNull();
+        assertThat(articleService).isNotNull();
     }
 
 }
