1a511c8eaa72aae3aebd9d61d64b7154bc1321de
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-25 17:22:51 -0300

    refactor(repository): enhance ArticleRepository with search functionality and entity graph
    - **ArticleRepository.java**:
      - Add `@EntityGraph` annotation to `findAllByAuthor_Username` to optimize fetching related entities.
      - Introduce `findAllByAuthor_UsernameAndSearchParams` method with `@EntityGraph` and `@Query` to enable searching articles by title, content, or subtitle for a specific author.
      - Add `@EntityGraph` annotation to `findByCategories_Name` to optimize fetching related entities.
      - Add `@EntityGraph` annotation to `findTop10ByOrderByCreatedAtDesc` to optimize fetching related entities.
    
    These changes improve the performance and functionality of the `ArticleRepository` by optimizing entity fetching and adding search capabilities.

--- server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/ArticleRepository.java
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/ArticleRepository.java
@@ -3,18 +3,32 @@ package com.barataribeiro.sentinelofliberty.repositories;
 import com.barataribeiro.sentinelofliberty.models.entities.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @EntityGraph(attributePaths = {"author", "categories", "references"})
     Page<Article> findAllByAuthor_Username(String username, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"author"})
+    @Query("""
+           SELECT a FROM Article a WHERE a.author.username = :username
+           AND (LOWER(a.title) LIKE LOWER(CONCAT('%', :term, '%'))
+                OR LOWER(a.content) LIKE LOWER(CONCAT('%', :term, '%'))
+                OR LOWER(a.subTitle) LIKE LOWER(CONCAT('%', :term, '%')))
+           """)
+    Page<Article> findAllByAuthor_UsernameAndSearchParams(@Param("username") String username,
+                                                          @Param("term") String term, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"author", "categories", "references"})
     Set<Article> findByCategories_Name(String name);
 
+    @EntityGraph(attributePaths = {"author", "categories", "references"})
     Set<Article> findTop10ByOrderByCreatedAtDesc();
 
     long countByTitle(String title);
-
-
 }
