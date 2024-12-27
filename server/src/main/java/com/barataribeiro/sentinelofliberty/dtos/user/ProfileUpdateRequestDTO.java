a561f8683d8d9176b7b569cc85a6d6310b31f3bc
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-27 09:51:06 -0300

    feat(user): add ProfileUpdateRequestDTO for user profile updates
    - **ProfileUpdateRequestDTO.java**:
      - Create `ProfileUpdateRequestDTO` class to handle user profile update requests.
      - Add fields for `currentPassword`, `username`, `email`, `displayName`, `fullName`, `avatarUrl`, `biography`, `location`, `website`, and `newPassword`.
      - Add validation annotations to ensure data integrity and format correctness.
    
    These changes introduce a new DTO to facilitate user profile updates, ensuring that the input data is properly validated.

---
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/dtos/user/ProfileUpdateRequestDTO.java
@@ -0,0 +1,53 @@
+package com.barataribeiro.sentinelofliberty.dtos.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import java.io.Serializable;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProfileUpdateRequestDTO implements Serializable {
+    @NotBlank
+    private String currentPassword;
+
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters.")
+    @Pattern(regexp = "^[a-z]*$", message = "Username must contain only lowercase letters.")
+    private String username;
+
+    @Email(regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}",
+           message = "You must provide a valid email address.")
+    private String email;
+
+    @Size(min = 3, max = 50, message = "Display name must be between 3 and 50 characters.")
+    @Pattern(regexp = "^[a-zA-Z ]*$", message = "Display name must contain only letters, and spaces.")
+    private String displayName;
+
+    @Pattern(regexp = "^[a-zA-Z ]*$", message = "Full name must contain only letters, and spaces.")
+    private String fullName;
+
+    @URL(message = "Invalid URL format.", protocol = "https",
+         regexp = "((((https?|ftps?|gopher|telnet|nntp)://)|(mailto:|news:))([-%()_.!~*';/?:@&=+$,A-Za-z0-9])+)")
+    private String avatarUrl;
+
+    @Size(max = 160, message = "Biography must be less than 160 characters.")
+    private String biography;
+
+    private String location;
+
+    @URL(message = "Invalid URL format.", protocol = "https",
+         regexp = "((((https?|ftps?|gopher|telnet|nntp)://)|(mailto:|news:))([-%()_.!~*';/?:@&=+$,A-Za-z0-9])+)")
+    private String website;
+
+    @Size(min = 8, max = 100, message = "New password must be between 8 and 100 characters.")
+    @Pattern(
+            message = "New password must contain at least one digit, one lowercase letter, one uppercase letter, one " +
+                    "special character and no whitespace.",
+            regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")
+    private String newPassword;
+}
