544cad05ba81cb348696c824fc72314e0b673355
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-06 06:30:59 -0300

    feat(suggestion): add method to delete suggestion by id and username
    - **SuggestionRepository.java**:
      - Add `deleteByIdAndUser_UsernameAllIgnoreCase` method to delete a suggestion by its id and the username of the user who created it.
    
    This change introduces a new method in the `SuggestionRepository` to allow deletion of suggestions based on both the suggestion id and the username of the user, enhancing the repository's functionality.

--- server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/SuggestionRepository.java
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/SuggestionRepository.java
@@ -4,4 +4,5 @@ import com.barataribeiro.sentinelofliberty.models.entities.Suggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
+    long deleteByIdAndUser_UsernameAllIgnoreCase(Long id, String username);
 }
