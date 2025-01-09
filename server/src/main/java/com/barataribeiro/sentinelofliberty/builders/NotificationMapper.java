42b417144f3cd6c542267187f721a5158a791398
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-09 06:59:35 -0300

    feat(notification): add NotificationMapper for mapping Notification entities to DTOs
    - **NotificationMapper.java**:
      - Create `NotificationMapper` class.
      - Add method `toNotificationDTO` to map `Notification` entity to `NotificationDTO`.
      - Add method `toNotificationDTOList` to map a list of `Notification` entities to a list of `NotificationDTOs`.
      - Use `ModelMapper` for object mapping.
      - Annotate the class with `@Component` and use `@RequiredArgsConstructor` for dependency injection.
    
    These changes introduce a mapper to convert `Notification` entities to their corresponding DTOs, facilitating data transfer between layers.

---
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/builders/NotificationMapper.java
@@ -0,0 +1,25 @@
+package com.barataribeiro.sentinelofliberty.builders;
+
+import com.barataribeiro.sentinelofliberty.models.entities.Notification;
+import com.barataribeiro.sentinelofliberty.models.entities.NotificationDTO;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class NotificationMapper {
+    private final ModelMapper modelMapper;
+
+    public NotificationDTO toNotificationDTO(Notification notification) {
+        return modelMapper.map(notification, NotificationDTO.class);
+    }
+
+    public List<NotificationDTO> toNotificationDTOList(@NotNull List<Notification> notifications) {
+        return notifications.stream().map(this::toNotificationDTO).toList();
+    }
+}
