70251c1142270c1d8ce4061e1b8c8092d81cf8c5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-13 06:34:09 -0300

    feat(token-repository): add method to count distinct tokens by value
    - Added `countDistinctByTokenValue` method to `TokenRepository` interface.
    
    This change introduces a method to count distinct tokens by their value, which is useful for validating token uniqueness and managing token-related operations.

--- server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/TokenRepository.java
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/TokenRepository.java
@@ -4,4 +4,5 @@ import com.barataribeiro.sentinelofliberty.models.entities.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TokenRepository extends JpaRepository<Token, String> {
+    int countDistinctByTokenValue(String tokenValue);
 }
