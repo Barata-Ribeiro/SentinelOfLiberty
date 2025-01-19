9dadb75dc853821f8045ff02dc7831c1be42f4b8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-19 10:32:12 -0300

    feat(auth): enhance session management and authentication flow
    - Refactored `session-verifier.tsx` to use `useSession` and `useEffect` for session verification
    - Added `route.ts` to handle GET and POST requests for authentication
    - Updated `token-refresh.ts` to include the URL for refreshing access tokens
    - Enhanced `auth.ts` to include proper URLs for login and token refresh, and improved session and JWT callbacks
    
    These changes improve the authentication flow by ensuring proper session management and handling token refresh errors more effectively.

--- client/src/helpers/session-verifier.tsx
+++ client/src/helpers/session-verifier.tsx
@@ -1,37 +1,31 @@
 "use client"
 
-import deleteSession           from "@/actions/application/delete-session"
-import { Route }               from "next"
-import { getSession, signOut } from "next-auth/react"
-import { useRouter }           from "next/navigation"
-import { use }                 from "react"
-
-async function fetchAndVerifySession() {
-    const session = await getSession()
-    
-    if (session?.error === "RefreshAccessTokenError") {
-        console.error("There was an error with the session, logging out...")
-        
-        try {
-            await deleteSession()
-        } catch (err) {
-            console.error(err)
-        } finally {
-            await signOut({ redirect: false })
-        }
-    }
-    
-    return session
-}
+import deleteSession              from "@/actions/application/delete-session"
+import { Route }                  from "next"
+import { signOut, useSession }    from "next-auth/react"
+import { useRouter }              from "next/navigation"
+import { useCallback, useEffect } from "react"
 
 export default function SessionVerifier() {
     const router = useRouter()
-    const session = use(fetchAndVerifySession())
+    const { data: session } = useSession()
     
-    if (!session) {
-        router.push(`${ window.location.origin }/auth/login` as Route<string>)
-        return null
-    }
+    const logout = useCallback(() => {
+        console.error("There was an error with the session, logging out...")
+        
+        deleteSession()
+            .then(response => console.log(response.message))
+            .catch(console.error)
+            .finally(async () => {
+                signOut({ redirect: false }).then(() => {
+                    router.push(`${ window.location.origin }/auth/login` as Route<string>)
+                })
+            })
+    }, [ router ])
+    
+    useEffect(() => {
+        if (session?.error === "RefreshAccessTokenError") logout()
+    }, [ session, logout ])
     
     return null
 }
