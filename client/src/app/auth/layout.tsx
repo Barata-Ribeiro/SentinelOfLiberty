6b9ef97984de43e0c88408afb2391b7918410eb1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-20 10:37:57 -0300

    feat(auth-layout): add AuthLayout component with background image
    - Created `AuthLayout` component to provide a layout for authentication pages
    - Applied background image and styles to `AuthLayout`
    - Included photo credit link in `AuthLayout`

---
+++ client/src/app/auth/layout.tsx
@@ -0,0 +1,37 @@
+import { getBackgroundImage } from "@/utils/functions"
+import { getImageProps }      from "next/image"
+import Link                   from "next/link"
+import { ReactNode }          from "react"
+import authPhoto              from "../../../public/auth-photo.jpg"
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+    const {
+        props: { srcSet },
+    } = getImageProps({ alt: "", width: 1700, height: 280, src: authPhoto })
+    const backgroundImage = getBackgroundImage(srcSet)
+    
+    return (
+        <main
+            className="flex min-h-screen w-auto flex-col items-center justify-center bg-stone-600"
+            style={ {
+                backgroundImage,
+                backgroundBlendMode: "multiply",
+                backgroundSize: "cover",
+                backgroundAttachment: "fixed",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+            } }>
+            { children }
+            <p className="text-center text-xs text-shadow-50/50">
+                Photo from Unsplash by{ " " }
+                <Link
+                    href="https://unsplash.com/@chesterfordhouse"
+                    rel="noreferrer noopener external"
+                    target="_blank"
+                    className="font-medium tracking-wide hover:text-shadow-200 hover:underline">
+                    Peter Lawrence
+                </Link>
+            </p>
+        </main>
+    )
+}
