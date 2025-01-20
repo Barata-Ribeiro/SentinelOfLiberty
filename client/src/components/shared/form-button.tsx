7c45d2544b9ec0cacbd61d210c6ea545b8b37227
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-20 10:37:13 -0300

    feat(form-components): add FormInput and FormButton components
    - Created `FormInput` component to handle input fields with labels and descriptions
    - Added support for disabled state in `FormInput`
    - Created `FormButton` component to handle form submission buttons with customizable styles
    
    These changes introduce reusable form components to improve consistency and maintainability in form handling.

---
+++ client/src/components/shared/form-button.tsx
@@ -0,0 +1,19 @@
+import tw                                  from "@/utils/tw"
+import { Button }                          from "@headlessui/react"
+import { ButtonHTMLAttributes, ReactNode } from "react"
+import { twMerge }                         from "tailwind-merge"
+
+interface FormButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children?: ReactNode
+    className?: string
+}
+
+export default function FormButton({ children, className, ...props }: FormButtonProps) {
+    const defaultStyles = tw`inline-flex items-center justify-center gap-x-2 rounded-md bg-marigold-600 px-3.5 py-2.5 text-center text-sm font-semibold text-marigold-50 shadow-sm transition duration-200 hover:bg-marigold-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-marigold-600 active:bg-marigold-700`
+    const styles = twMerge(defaultStyles, className)
+    return (
+        <Button className={ styles } type="submit" { ...props }>
+            { children }
+        </Button>
+    )
+}
---
+++ client/src/components/shared/form-input.tsx
@@ -0,0 +1,29 @@
+import { Description, Field, Input, Label } from "@headlessui/react"
+import { InputHTMLAttributes }              from "react"
+
+interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    disabledField?: boolean
+    label: string
+    description?: string
+}
+
+export default function FormInput({ label, description, disabledField, ...props }: FormInputProps) {
+    return (
+        <div className="w-full min-w-[200px] max-w-sm">
+            <Field className="relative" disabled={ disabledField }>
+                <Input
+                    className="bg-transparent ease data-[disabled]:bg-gray-100 peer w-full rounded-md border border-stone-200 px-3 py-2 text-sm text-stone-700 shadow-sm transition duration-300 placeholder:text-stone-400 hover:border-stone-300 focus:border-stone-400 focus:shadow focus:outline-none focus:ring-1 focus:ring-stone-400/50 data-[disabled]:pointer-events-none"
+                    { ...props }
+                />
+                <Label className="absolute left-2.5 top-2.5 origin-left transform cursor-text bg-white px-1 text-sm text-stone-400 transition-all peer-focus:-top-2 peer-focus:left-2.5 peer-focus:scale-90 peer-focus:text-xs peer-focus:text-stone-400 data-[disabled]:pointer-events-none data-[disabled]:select-none data-[disabled]:opacity-50">
+                    { label }
+                </Label>
+                { description && (
+                    <Description className="data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+                        { description }
+                    </Description>
+                ) }
+            </Field>
+        </div>
+    )
+}
