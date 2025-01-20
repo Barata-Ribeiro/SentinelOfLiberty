31dda0560afb6d0af3e81c07c68779690494bb8d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-20 05:40:51 -0300

    feat(footer): add footer component
    - Created `Footer` component with navigation links to GitHub and portfolio
    - Included logo and copyright information

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,46 @@
+import Image                 from "next/image"
+import Link                  from "next/link"
+import { FaGithub, FaGlobe } from "react-icons/fa6"
+import miniLogo              from "../../public/sentinel-of-liberty-S-final.svg"
+
+const navigation = [
+    {
+        name: "GitHub",
+        href: "https://github.com/Barata-Ribeiro/SentinelOfLiberty",
+        icon: FaGithub,
+    },
+    {
+        name: "Portfolio",
+        href: "https://barataribeiro.com/",
+        icon: FaGlobe,
+    },
+]
+
+export default function Footer() {
+    return (
+        <footer className="bg-stone-50">
+            <div className="container px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+                <div className="flex justify-center space-x-6 md:order-2">
+                    { navigation.map(item => (
+                        <Link key={ item.name }
+                              href={ item.href }
+                              title={ item.name }
+                              rel="noopener noreferrer external"
+                              target="_blank"
+                              className="text-stone-400 hover:text-stone-500">
+                            <span className="sr-only">{ item.name }</span>
+                            <item.icon aria-hidden="true" className="h-6 w-6" />
+                        </Link>
+                    )) }
+                </div>
+                <div className="mt-8 flex items-center gap-x-3 divide-stone-200 md:order-1 md:mt-0">
+                    <Image src={ miniLogo } alt="" className="size-8" />
+                    <span aria-hidden="true" className="bg-stone-200 h-6 w-px"></span>
+                    <p className="text-center text-xs leading-5 text-stone-500">
+                        &copy; { new Date().getFullYear() } Sentinel of Liberty. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
