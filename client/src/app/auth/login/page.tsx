a75d83d95cd23fa06adf3e7743e3835c35f0a1f2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-21 09:44:01 -0300

    feat(auth-login): add login page and form components
    - Created `LoginPage` component to provide a login interface
    - Added `LoginForm` component with form fields for username, password, and remember me option
    - Implemented form validation and error handling using `ApplicationRequestFormError` and `InputValidationError` components
    - Included `Spinner` and `FormButton` components for form submission feedback
    - Added navigation links for password reset and account creation
    
    These changes introduce a new login page and form, enhancing the authentication flow and user experience.

---
+++ client/src/app/auth/login/page.tsx
@@ -0,0 +1,34 @@
+import LoginForm    from "@/components/forms/login-form"
+import { Metadata } from "next"
+import Link         from "next/link"
+
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Login to your account",
+}
+
+export default function LoginPage() {
+    return (
+        <section className="mb-20 w-full sm:container">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+                <div className="bg-white px-6 py-12 shadow sm:rounded-md sm:px-12">
+                    <div className="mb-8">
+                        <h4 className="block text-xl font-medium text-stone-800">Login</h4>
+                        <p className="text-sm font-light text-stone-500">Welcome back! Please login to your account.</p>
+                    </div>
+                    
+                    <LoginForm />
+                </div>
+
+                <p className="mt-10 text-center text-sm text-stone-300">
+                    Not a member?{ " " }
+                    <Link
+                        href="/auth/register"
+                        className="font-semibold leading-6 text-marigold-600 hover:text-marigold-500 active:text-marigold-700">
+                        Create an account
+                    </Link>
+                </p>
+            </div>
+        </section>
+    )
+}
---
+++ client/src/components/forms/login-form.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { ProblemDetails, State }         from "@/@types/application"
+import postAuthLogin                     from "@/actions/auth/post-auth-login"
+import ApplicationRequestFormError       from "@/components/feedback/application-request-form-error"
+import InputValidationError              from "@/components/feedback/input-validation-error"
+import Spinner                           from "@/components/helpers/spinner"
+import FormButton                        from "@/components/shared/form-button"
+import FormInput                         from "@/components/shared/form-input"
+import { Field, Fieldset, Input, Label } from "@headlessui/react"
+import Link                              from "next/link"
+import { useActionState }                from "react"
+import { FaArrowRightToBracket }         from "react-icons/fa6"
+
+const initialState: State = {
+    ok: false,
+    error: null,
+    response: null,
+}
+
+export default function LoginForm() {
+    const [ formState, formAction, pending ] = useActionState(postAuthLogin, initialState)
+    
+    return (
+        <form action={ formAction } className="space-y-6">
+            <FormInput label="Username" type="text" name="username" autoComplete="username" required />
+            <FormInput label="Password" type="password" name="password" autoComplete="current-password" required />
+
+            <Fieldset className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+                <Field className="flex items-center">
+                    <Input
+                        id="rememberMe"
+                        name="rememberMe"
+                        type="checkbox"
+                        className="size-4 rounded border-stone-300 text-marigold-600 focus:ring-marigold-600"
+                    />
+                    <Label htmlFor="rememberMe" className="ml-3 block text-sm leading-6 text-stone-900">
+                        Remember me
+                    </Label>
+                </Field>
+
+                <div className="text-sm leading-6">
+                    <Link
+                        href="/auth/reset-password"
+                        className="font-semibold text-marigold-600 hover:text-marigold-500 active:text-marigold-700">
+                        Forgot password?
+                    </Link>
+                </div>
+            </Fieldset>
+            
+            { formState.error && !Array.isArray(formState.error) && (
+                <ApplicationRequestFormError error={ formState.error as ProblemDetails } />
+            ) }
+            
+            { formState.error && Array.isArray(formState.error) && <InputValidationError errors={ formState.error } /> }
+            
+            <FormButton className="w-full" disabled={ pending }>
+                { pending ? (
+                    <>
+                        Login <FaArrowRightToBracket aria-hidden="true" className="size-4" />
+                    </>
+                ) : (
+                      <>
+                        <Spinner /> Loading...
+                    </>
+                  ) }
+            </FormButton>
+        </form>
+    )
+}
