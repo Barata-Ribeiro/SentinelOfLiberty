8504692ba6a24a99c9eca156d879529835b172fa
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-21 09:44:50 -0300

    feat(auth): add server-side login handler
    - Created `postAuthLogin` function to handle login form submissions on the server
    - Validates form data using `authLoginSchema` from Zod
    - Handles different error scenarios using `ResponseError` and `problemDetailsFactory`
    - Uses `signIn` function to authenticate user credentials
    - Redirects to user dashboard upon successful login
    
    These changes introduce a server-side handler for processing login requests, improving authentication flow and error handling.

---
+++ client/src/actions/auth/post-auth-login.ts
@@ -0,0 +1,44 @@
+"use server"
+
+import "server-only"
+import { State }                 from "@/@types/application"
+import ResponseError             from "@/actions/application/response-error"
+import { signIn }                from "@/auth"
+import { authLoginSchema }       from "@/helpers/zod-schemas"
+import { problemDetailsFactory } from "@/utils/functions"
+import { redirect }              from "next/navigation"
+
+export default async function postAuthLogin(state: State, formData: unknown) {
+    try {
+        if (!(formData instanceof FormData)) {
+            return ResponseError(
+                problemDetailsFactory({
+                                          type: "https://httpstatuses.com/400",
+                                          title: "Invalid Form Data",
+                                          status: 400,
+                                          detail: "Invalid form data was submitted. Please try again.",
+                                          instance: "/auth/login",
+                                      }),
+            )
+        }
+        
+        const rawFormData = Object.fromEntries(formData.entries())
+        const parsedFormData = authLoginSchema.safeParse(rawFormData)
+        
+        if (!parsedFormData.success) {
+            return ResponseError(parsedFormData.error)
+        }
+        
+        await signIn("credentials", {
+            username: parsedFormData.data.username,
+            password: parsedFormData.data.password,
+            rememberMe: parsedFormData.data.rememberMe,
+            redirect: false,
+        })
+        
+        redirect(`/dashboard/${ parsedFormData.data.username }`)
+    } catch (error) {
+        console.log("CALLBACK ERROR: ", error)
+        return ResponseError(error)
+    }
+}
