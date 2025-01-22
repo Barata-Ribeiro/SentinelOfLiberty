8bd2075b109a26ded053b8e57d349a613f04c661
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-22 09:57:06 -0300

    refactor(tailwind): update color configuration
    - Imported `colors` from `tailwindcss/colors`
    - Updated `colors` configuration to use predefined Tailwind CSS colors
    - Removed custom color definitions for `white`, `black`, and `stone`
    - Retained custom color definition for `shadow`
    
    These changes simplify the color configuration by leveraging Tailwind CSS's built-in color palette, improving maintainability and consistency.

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,31 +1,25 @@
 import type { Config } from "tailwindcss"
+import colors          from "tailwindcss/colors"
 
 export default {
     content: [ "./src/**/*.{js,ts,jsx,tsx,mdx}" ],
     theme: {
         container: { center: true, padding: "1rem" },
-        colors: {},
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: colors.black,
+            white: colors.white,
+            stone: colors.stone,
+            red: colors.red,
+            yellow: colors.yellow,
+        },
         extend: {
             fontFamily: {
                 heading: [ "var(--font-lora)", "serif" ],
                 body: [ "var(--font-merriWeather)", "serif" ],
             },
             colors: {
-                white: "white",
-                black: "black",
-                "stone": {
-                    "50": "#fafaf9",
-                    "100": "#f5f5f4",
-                    "200": "#e7e5e4",
-                    "300": "#d6d3d1",
-                    "400": "#a8a29e",
-                    "500": "#78716c",
-                    "600": "#57534e",
-                    "700": "#44403c",
-                    "800": "#292524",
-                    "900": "#1c1917",
-                    "950": "#0c0a09",
-                }, // NEUTRAL
                 "shadow": {
                     "50": "#f6f5f0",
                     "100": "#e8e8d9",
