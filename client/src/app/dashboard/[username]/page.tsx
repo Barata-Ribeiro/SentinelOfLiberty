ff087091a12b6e51af1cdce331c16b65c75e105d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-25 11:51:33 -0300

    feat(dashboard): enhance dashboard page with metadata and improved layout
    - Added `generateMetadata` function to set dynamic page title and description based on the username.
    - Updated layout styles in `DashboardHomePage`:
      - Adjusted grid gaps for different screen sizes.
      - Improved button alignment and responsiveness.
      - Enhanced site interactions list with better styling and structure.
    
    These changes improve the user dashboard by providing dynamic metadata for better SEO and a more responsive, user-friendly layout.

--- client/src/app/dashboard/[username]/page.tsx
+++ client/src/app/dashboard/[username]/page.tsx
@@ -9,6 +9,14 @@ interface DashboardHomePageProps {
     params: Promise<{ username: string }>
 }
 
+export async function generateMetadata({ params }: Readonly<DashboardHomePageProps>) {
+    const username = (await params).username
+    return {
+        title: `${ username }`,
+        description: "This is the dashboard home page",
+    }
+}
+
 export default async function DashboardHomePage({ params }: Readonly<DashboardHomePageProps>) {
     const username = (await params).username
     if (!username) return notFound()
@@ -29,8 +37,8 @@ export default async function DashboardHomePage({ params }: Readonly<DashboardHo
     console.groupEnd()
     
     return (
-        <div className="my-8 grid grid-cols-1 gap-8 md:grid-cols-[33%_66%]">
-            <aside className="grid gap-y-8 *:rounded-md *:border-2 *:border-stone-200 *:p-4">
+        <div className="my-8 grid grid-cols-1 gap-2 sm:gap-4 md:grid-cols-[33%_66%] lg:gap-8">
+            <aside className="grid gap-y-2 *:rounded-md *:border-2 *:border-stone-200 *:p-4 sm:gap-y-4 lg:gap-y-8">
                 <div className="grid place-items-center gap-y-4">
                     <Avatar name={ username } size={ 96 } src={ profile.avatarUrl } />
 
@@ -62,42 +70,58 @@ export default async function DashboardHomePage({ params }: Readonly<DashboardHo
                         </span>
                       ) }
                     
-                    <div className="mt-4 flex space-x-3 lg:mt-6">
+                    <div className="my-4 flex flex-col items-center justify-center gap-4 lg:mt-6 lg:flex-row">
                         <Button
                             type="button"
-                            className="bg-marigold-600 hover:bg-marigold-700 active:bg-marigold-800 focus:ring-marigold-200 text-marigold-50 inline-flex cursor-pointer items-center rounded-lg px-4 py-2 text-center text-sm font-medium select-none focus:ring-4 focus:outline-none"
+                            className="bg-marigold-600 hover:bg-marigold-700 active:bg-marigold-800 focus:ring-marigold-200 text-marigold-50 inline-flex w-max cursor-pointer items-center rounded-lg px-4 py-2 text-center text-sm font-medium select-none focus:ring-4 focus:outline-none"
                             aria-label="Edit profile">
                             Edit Profile
                         </Button>
 
                         <Button
                             type="button"
-                            className="text-shadow-900 focus:ring-marigold-200 inline-flex cursor-pointer items-center rounded-lg border border-stone-300 bg-white px-4 py-2 text-center text-sm font-medium select-none hover:bg-stone-100 focus:ring-4 focus:outline-none active:bg-stone-200"
+                            className="text-shadow-900 focus:ring-marigold-200 inline-flex w-max cursor-pointer items-center rounded-lg border border-stone-300 bg-white px-4 py-2 text-center text-sm font-medium select-none hover:bg-stone-100 focus:ring-4 focus:outline-none active:bg-stone-200"
                             aria-label="Check account details">
                             Account Details
                         </Button>
                     </div>
-                    <div className="mt-5 border-t-2 border-stone-100 pt-3">
+
+                    <div className="border-t-2 border-stone-100 pt-3">
                         <h3 className="text-shadow-900 mb-2 text-center text-xl font-medium">Biography</h3>
                         <p className="text-shadow-600 text-center text-sm">
                             { profile.biography ?? "No biography yet. Edit you profile above!!" }
                         </p>
                     </div>
                 </div>
+
                 <div>
                     <h3 className="text-shadow-900 mb-2 self-center text-xl font-medium">Site Interactions</h3>
-                    <ul className="list-disc space-y-2 pl-5">
-                        <li className="font-medium">
-                            Suggestions Made:
-                            <span className="font-normal">{ dashboard.totalWrittenSuggestions }</span>
+                    <ul
+                        className="space-y-2 divide-y divide-stone-200 *:px-4 *:sm:px-6"
+                        aria-label="List of site interactions">
+                        <li className="pb-2">
+                            <p className="text-shadow-400 text-sm leading-6 font-medium">Suggestions Made</p>
+                            <p className="mt-2 flex items-baseline gap-x-2">
+                                <span className="text-shadow-900 text-4xl font-semibold tracking-tight">
+                                    { dashboard.totalWrittenSuggestions }
+                                </span>
+                            </p>
                         </li>
-                        <li className="font-medium">
-                            Articles Published:
-                            <span className="font-normal">{ dashboard.totalWrittenArticles }</span>
+                        <li className="pb-2">
+                            <p className="text-shadow-400 text-sm leading-6 font-medium">Articles Published</p>
+                            <p className="mt-2 flex items-baseline gap-x-2">
+                                <span className="text-shadow-900 text-4xl font-semibold tracking-tight">
+                                    { dashboard.totalWrittenArticles }
+                                </span>
+                            </p>
                         </li>
-                        <li className="font-medium">
-                            Comments Written:
-                            <span className="font-normal">{ dashboard.totalWrittenComments }</span>
+                        <li>
+                            <p className="text-shadow-400 text-sm leading-6 font-medium">Comments Written</p>
+                            <p className="mt-2 flex items-baseline gap-x-2">
+                                <span className="text-shadow-900 text-4xl font-semibold tracking-tight">
+                                    { dashboard.totalWrittenComments }
+                                </span>
+                            </p>
                         </li>
                     </ul>
                 </div>
