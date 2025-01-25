233226d0748f6f2db9c18ab2b83c55a9a3a3b019
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-25 09:53:06 -0300

    test(user-controller): add integration tests for user dashboard information
    - Added `SuggestionRepository` to `UserControllerTestIT` for accessing suggestion data.
    - Added `getOwnDashboardInformationForAdminUser` test to verify the retrieval of dashboard information for a logged-in admin user.
    - Added `getOwnDashboardInformationForRegularUser` test to verify the retrieval of dashboard information for a logged-in regular user.
    - Updated assertions to check the correctness of the dashboard data, including latest written article, suggestions, comments, and total counts.
    
    These changes enhance the test coverage for user dashboard information retrieval.

--- server/src/test/java/com/barataribeiro/sentinelofliberty/controllers/UserControllerTestIT.java
+++ server/src/test/java/com/barataribeiro/sentinelofliberty/controllers/UserControllerTestIT.java
@@ -3,6 +3,7 @@ package com.barataribeiro.sentinelofliberty.controllers;
 import com.barataribeiro.sentinelofliberty.exceptions.throwables.EntityNotFoundException;
 import com.barataribeiro.sentinelofliberty.repositories.ArticleRepository;
 import com.barataribeiro.sentinelofliberty.repositories.CommentRepository;
+import com.barataribeiro.sentinelofliberty.repositories.SuggestionRepository;
 import com.barataribeiro.sentinelofliberty.repositories.UserRepository;
 import com.barataribeiro.sentinelofliberty.utils.ApplicationBaseIntegrationTest;
 import com.jayway.jsonpath.JsonPath;
@@ -17,8 +18,11 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.List;
+
 import static com.barataribeiro.sentinelofliberty.utils.ApplicationTestConstants.ADMIN_UPDATE_PROFILE_PAYLOAD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @DirtiesContext
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -29,6 +33,7 @@ class UserControllerTestIT extends ApplicationBaseIntegrationTest {
     private final MockMvcTester mockMvcTester;
     private final UserRepository userRepository;
     private final ArticleRepository articleRepository;
+    private final SuggestionRepository suggestionRepository;
     private final CommentRepository commentRepository;
 
     @Test
@@ -69,6 +74,50 @@ class UserControllerTestIT extends ApplicationBaseIntegrationTest {
                          assertEquals("testuser", JsonPath.read(data, "$.username"));
                          assertEquals((int) commentRepository.countDistinctByUser_Username("testuser"),
                                       (int) JsonPath.read(data, "$.commentsCount"));
+                     });
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Get own dashboard information for logged in admin user")
+    void getOwnDashboardInformationForAdminUser() {
+        mockMvcTester.get().uri(BASE_URL + "/me/dashboard")
+                     .headers(authHeader())
+                     .assertThat()
+                     .hasStatusOk()
+                     .bodyJson()
+                     .extractingPath("$.data")
+                     .satisfies(data -> {
+                         assertEquals("testadmin", JsonPath.read(data, "$.latestWrittenArticle.author.username"));
+                         assertEquals(0, ((List<?>) JsonPath.read(data, "$.latestThreeSuggestions")).size());
+                         assertEquals(0, ((List<?>) JsonPath.read(data, "$.latestThreeComments")).size());
+
+                         assertEquals((int) articleRepository.countDistinctByAuthor_Username("testadmin"),
+                                      (int) JsonPath.read(data, "$.totalWrittenArticles"));
+                         assertEquals((int) suggestionRepository.countDistinctByUser_Username("testadmin"),
+                                      (int) JsonPath.read(data, "$.totalWrittenSuggestions"));
+                         assertEquals((int) commentRepository.countDistinctByUser_Username("testadmin"),
+                                      (int) JsonPath.read(data, "$.totalWrittenComments"));
+                     });
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Get own dashboard information for logged in regular user")
+    void getOwnDashboardInformationForRegularUser() {
+        mockMvcTester.get().uri(BASE_URL + "/me/dashboard")
+                     .headers(userAuthHeader())
+                     .assertThat()
+                     .hasStatusOk()
+                     .bodyJson()
+                     .extractingPath("$.data")
+                     .satisfies(data -> {
+                         assertNull(JsonPath.read(data, "$.latestWrittenArticle"));
+                         assertEquals(3, ((List<?>) JsonPath.read(data, "$.latestThreeSuggestions")).size());
+                         assertEquals(0, ((List<?>) JsonPath.read(data, "$.latestThreeComments")).size());
+                         assertEquals(0, (int) JsonPath.read(data, "$.totalWrittenArticles"));
+                         assertEquals(71, (int) JsonPath.read(data, "$.totalWrittenSuggestions"));
+                         assertEquals(0, (int) JsonPath.read(data, "$.totalWrittenComments"));
                      });
     }
 
