4416f3138053a437b09774fd49aa1a3abe911e07
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-25 07:23:19 -0300

    feat(profile): add functionality to fetch own profile data
    - Created `get-own-profile.ts`:
      - Implemented `getOwnProfile` function to fetch the authenticated user's profile data from the server
      - Added error handling and response validation
    
    These changes introduce the ability to fetch the authenticated user's profile data, enhancing the user experience with personalized information.

---
+++ client/src/actions/users/get-own-profile.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import { ProblemDetails, RestResponse } from "@/@types/application"
+import ResponseError                    from "@/actions/application/response-error"
+import { getOwnProfileUrl }             from "@/utils/routes"
+import { auth }                         from "auth"
+
+export default async function getOwnProfile() {
+    const session = await auth()
+    
+    try {
+        const URL = getOwnProfileUrl()
+        
+        const response = await fetch(URL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ session?.accessToken }`,
+            },
+            next: { revalidate: 60, tags: [ "profile", "user-settings", "dashboard" ] },
+        })
+        
+        const json = await response.json()
+        
+        if (!response.ok) {
+            const problemDetails = json as ProblemDetails
+            return ResponseError(problemDetails)
+        }
+        
+        return {
+            ok: true,
+            error: null,
+            response: json as RestResponse,
+        }
+    } catch (error) {
+        return ResponseError(error)
+    }
+}
