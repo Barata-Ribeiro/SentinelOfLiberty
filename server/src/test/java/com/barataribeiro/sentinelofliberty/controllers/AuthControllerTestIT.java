4b207f920517fcc03f8ba8696c69990d5435aaed
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-26 07:16:28 -0300

    test(auth-controller): refactor integration tests to use MockMvcTester
    - Replaced `MockMvc` with `MockMvcTester` for improved test readability and maintainability.
    - Updated `testLoginWithValidCredentials` to use `MockMvcTester` for verifying successful login.
    - Updated `testLoginWithInvalidCredentials` to use `MockMvcTester` for verifying login failure with invalid credentials.
    - Updated `testInvalidLoginScenarios` to use `MockMvcTester` for verifying various invalid login scenarios.
    - Updated `testRegistrationWithConcurrency` to use `MockMvcTester` for handling high-concurrency registration.
    - Updated `testTokenRefreshFlow` to use `MockMvcTester` for verifying token refresh flow.
    - Updated `logoutWithValidRefreshToken` to use `MockMvcTester` for verifying logout with a valid refresh token.
    - Updated `logoutWithInvalidRefreshToken` to use `MockMvcTester` for verifying logout with an invalid refresh token.
    
    These changes enhance the test coverage and ensure proper handling of various authentication scenarios using `MockMvcTester`.

--- server/src/test/java/com/barataribeiro/sentinelofliberty/controllers/AuthControllerTestIT.java
+++ server/src/test/java/com/barataribeiro/sentinelofliberty/controllers/AuthControllerTestIT.java
@@ -8,24 +8,24 @@ import com.barataribeiro.sentinelofliberty.utils.ApplicationBaseIntegrationTest;
 import com.barataribeiro.sentinelofliberty.utils.ConcurrencyTestUtil;
 import com.jayway.jsonpath.JsonPath;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 import static com.barataribeiro.sentinelofliberty.utils.ApplicationTestConstants.VALID_LOGIN_PAYLOAD;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DirtiesContext
@@ -33,72 +33,87 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthControllerTestIT extends ApplicationBaseIntegrationTest {
     private static final String BASE_URL = "/api/v1/auth";
 
-    private final MockMvc mockMvc;
+    private final MockMvcTester mockMvcTester;
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
 
     @Test
     @DisplayName("Test login with valid request body and an existing user attempts to login with valid credentials")
-    void testLoginWithValidCredentials() throws Exception {
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(VALID_LOGIN_PAYLOAD))
-               .andExpect(status().isOk())
-               .andDo(print());
+    void testLoginWithValidCredentials() {
+        mockMvcTester.post().uri(BASE_URL + "/login")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content(VALID_LOGIN_PAYLOAD)
+                     .assertThat().hasStatusOk()
+                     .bodyJson()
+                     .satisfies(jsonContent -> {
+                         assertEquals("You have successfully logged in",
+                                      JsonPath.read(jsonContent.getJson(), "$.message"));
+                         assertEquals("testuser", JsonPath.read(jsonContent.getJson(), "$.data.user.username"));
+                     });
     }
 
     @Test
     @DisplayName("Test login with valid request body and an existing user attempts to login with invalid credentials")
-    void testLoginWithInvalidCredentials() throws Exception {
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(
-                                        "{\"username\": \"testuser\", \"password\": \"wrongpassword\", " +
-                                                "\"rememberMe\": true}"))
-               .andExpect(status().isUnauthorized())
-               .andDo(print())
-               .andExpect(result -> assertInstanceOf(InvalidCredentialsException.class, result.getResolvedException()));
+    void testLoginWithInvalidCredentials() {
+        mockMvcTester.post().uri(BASE_URL + "/login")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content("""
+                                {
+                                    "username": "testuser",
+                                    "password": "wrongpassword",
+                                    "rememberMe": true
+                                }
+                              """)
+                     .assertThat().hasStatus4xxClientError().hasStatus(HttpStatus.UNAUTHORIZED)
+                     .failure().isInstanceOf(InvalidCredentialsException.class);
     }
 
     @Test
     @DisplayName("Test login scenarios where user does not exist; the body is empty; or the boolean value is invalid")
-    void testInvalidLoginScenarios() throws Exception {
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(
-                                        "{\"username\": \"test\", \"password\": \"test\", " +
-                                                "\"rememberMe\": true}"))
-               .andDo(print())
-               .andExpect(status().isNotFound())
-               .andExpect(result -> assertInstanceOf(EntityNotFoundException.class, result.getResolvedException()));
+    void testInvalidLoginScenarios() {
+        mockMvcTester.post().uri(BASE_URL + "/login")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content("""
+                                {
+                                    "username": "test",
+                                    "password": "test",
+                                    "rememberMe": "true"
+                                }
+                              """).assertThat().hasStatus4xxClientError().hasStatus(HttpStatus.NOT_FOUND)
+                     .failure().isInstanceOf(EntityNotFoundException.class);
 
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("{}"))
-               .andExpect(status().isBadRequest())
-               .andDo(print())
-               .andExpect(result -> assertInstanceOf(MethodArgumentNotValidException.class,
-                                                     result.getResolvedException()));
+        mockMvcTester.post().uri(BASE_URL + "/login")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content("{}")
+                     .assertThat().hasStatus4xxClientError().hasStatus(HttpStatus.BAD_REQUEST)
+                     .failure().isInstanceOf(MethodArgumentNotValidException.class);
 
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("{\"username\": \"test\", \"password\": \"test\", \"rememberMe\": \"test\"}"))
-               .andExpect(status().isBadRequest())
-               .andDo(print())
-               .andExpect(result -> assertInstanceOf(HttpMessageNotReadableException.class,
-                                                     result.getResolvedException()));
+        mockMvcTester.post().uri(BASE_URL + "/login")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content("""
+                                {
+                                    "username": "test",
+                                    "password": "test",
+                                    "rememberMe": "test"
+                                }
+                              """).assertThat().hasStatus4xxClientError().hasStatus(HttpStatus.BAD_REQUEST)
+                     .failure().isInstanceOf(HttpMessageNotReadableException.class);
     }
 
     @Test
     @DisplayName("Test registration where high-concurrency is expected and the user is registered only once")
-    void testRegistrationWithConcurrency() {
+    void testRegistrationWithConcurrency(@Autowired @NotNull MockMvc mockMvc) {
         ConcurrencyTestUtil.doAsyncAndConcurrently(10, () -> mockMvc
                 .perform(post(BASE_URL + "/register")
                                  .contentType(MediaType.APPLICATION_JSON)
-                                 .content("{\"username\": \"jasonbourne\", " +
-                                                  "\"email\": \"jasonbourne@cia.com\", " +
-                                                  "\"password\": \"q@7$eEMvmVz7!fDn\", " +
-                                                  "\"displayName\": \"Jason Bourne\"}"))
+                                 .content("""
+                                          {
+                                            "username": "jasonbourne",
+                                            "email": "jasonbourne@cia.com",
+                                            "password": "q@7$eEMvmVz7!fDn",
+                                            "displayName": "Jason Bourne"
+                                          }
+                                          """))
                 .andExpect(status().isCreated())
                 .andDo(print()));
 
@@ -108,54 +123,53 @@ class AuthControllerTestIT extends ApplicationBaseIntegrationTest {
 
     @Test
     @DisplayName("Test token refresh flow with both valid and invalid refresh tokens")
-    void testTokenRefreshFlow() throws Exception {
-        mockMvc.perform(post(BASE_URL + "/refresh-token")
-                                .header("X-Refresh-Token", "invalid-token"))
-               .andExpect(status().isUnauthorized())
-               .andDo(print())
-               .andExpect(jsonPath("$.detail")
-                                  .value("The provided token is invalid"));
+    void testTokenRefreshFlow() {
+        mockMvcTester.post().uri(BASE_URL + "/refresh-token")
+                     .header("X-Refresh-Token", "invalid-token")
+                     .assertThat().hasStatus4xxClientError().hasStatus(HttpStatus.UNAUTHORIZED)
+                     .failure().isInstanceOf(InvalidCredentialsException.class);
 
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(VALID_LOGIN_PAYLOAD))
-               .andExpect(status().isOk())
-               .andDo(print())
-               .andExpect(jsonPath("$.data.accessToken").exists())
-               .andExpect(jsonPath("$.data.refreshToken").exists())
-               .andDo(result -> {
-                   String responseBody = result.getResponse().getContentAsString();
-                   String refreshToken = JsonPath.read(responseBody, "$.data.refreshToken");
-                   mockMvc.perform(post(BASE_URL + "/refresh-token")
-                                           .header("X-Refresh-Token", refreshToken))
-                          .andExpect(status().isOk())
-                          .andDo(print())
-                          .andExpect(jsonPath("$.data.accessToken").exists())
-                          .andExpect(jsonPath("$.data.refreshToken").doesNotExist());
-               });
+        mockMvcTester.post().uri(BASE_URL + "/login").contentType(MediaType.APPLICATION_JSON)
+                     .content(VALID_LOGIN_PAYLOAD).assertThat().hasStatusOk()
+                     .bodyJson().extractingPath("$.data")
+                     .satisfies(data -> {
+                         assertNotNull(JsonPath.read(data, "$.accessToken"));
+                         assertNotNull(JsonPath.read(data, "$.refreshToken"));
+                         String refreshToken = JsonPath.read(data, "$.refreshToken");
+
+                         mockMvcTester.post().uri(BASE_URL + "/refresh-token")
+                                      .header("X-Refresh-Token", refreshToken)
+                                      .assertThat().hasStatusOk()
+                                      .bodyJson()
+                                      .extractingPath("$.data")
+                                      .satisfies(newData -> {
+                                          assertNotNull(JsonPath.read(newData, "$.accessToken"));
+                                          assertNull(JsonPath.read(newData, "$.refreshToken"),
+                                                     "Refresh token not be returned");
+                                      });
+                     });
     }
 
     @Test
     @DisplayName("Test logout with valid refresh token")
-    void logoutWithValidRefreshToken() throws Exception {
+    void logoutWithValidRefreshToken() {
         AtomicReference<String> refreshToken = new AtomicReference<>();
 
-        mockMvc.perform(post(BASE_URL + "/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(VALID_LOGIN_PAYLOAD))
-               .andExpect(status().isOk())
-               .andDo(print())
-               .andExpect(jsonPath("$.data.refreshToken").exists())
-               .andDo(result -> {
-                   String responseBody = result.getResponse().getContentAsString();
-                   refreshToken.set(JsonPath.read(responseBody, "$.data.refreshToken"));
-               });
+        mockMvcTester.post().uri(BASE_URL + "/login").contentType(MediaType.APPLICATION_JSON)
+                     .content(VALID_LOGIN_PAYLOAD).assertThat().hasStatusOk()
+                     .bodyJson().extractingPath("$.data")
+                     .satisfies(data -> {
+                         assertNotNull(JsonPath.read(data, "$.accessToken"));
+                         assertNotNull(JsonPath.read(data, "$.refreshToken"));
+                         refreshToken.set(JsonPath.read(data, "$.refreshToken"));
+                     });
 
-        mockMvc.perform(delete(BASE_URL + "/logout")
-                                .header("X-Refresh-Token", refreshToken.get()))
-               .andExpect(status().isNoContent())
-               .andDo(print())
-               .andExpect(jsonPath("$.message").value("You have successfully logged out"));
+        mockMvcTester.delete().uri(BASE_URL + "/logout")
+                     .header("X-Refresh-Token", refreshToken.get())
+                     .assertThat().hasStatus2xxSuccessful().hasStatus(HttpStatus.NO_CONTENT)
+                     .bodyJson()
+                     .satisfies(jsonContent -> assertEquals("You have successfully logged out",
+                                                            JsonPath.read(jsonContent.getJson(), "$.message")));
 
         assertEquals(1, tokenRepository.countDistinctByTokenValue(refreshToken.get()),
                      "The refresh token should be invalidated after logout");
@@ -163,11 +177,10 @@ class AuthControllerTestIT extends ApplicationBaseIntegrationTest {
 
     @Test
     @DisplayName("Test logout with invalid refresh token")
-    void logoutWithInvalidRefreshToken() throws Exception {
-        mockMvc.perform(delete(BASE_URL + "/logout")
-                                .header("X-Refresh-Token", "invalid-refresh-token"))
-               .andExpect(status().isUnauthorized())
-               .andDo(print())
-               .andExpect(result -> assertInstanceOf(InvalidCredentialsException.class, result.getResolvedException()));
+    void logoutWithInvalidRefreshToken() {
+        mockMvcTester.delete().uri(BASE_URL + "/logout")
+                     .header("X-Refresh-Token", "invalid-refresh-token")
+                     .assertThat().hasStatus4xxClientError().hasStatus(HttpStatus.UNAUTHORIZED)
+                     .failure().isInstanceOf(InvalidCredentialsException.class);
     }
 }
