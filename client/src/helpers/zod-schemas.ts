fe17ccfa4a482d28537b003b52589ec0aaff277a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-28 10:17:26 -0300

    feat(profile): add user profile update schema and patch profile action
    - Added `userProfileUpdateSchema` to validate and transform user profile update data:
      - Includes validation for `username`, `displayName`, `email`, `currentPassword`, `newPassword`, and `confirmNewPassword`.
      - Ensures `newPassword` and `confirmNewPassword` match.
      - Transforms data to remove empty fields.
    
    - Created `patchOwnProfile` action to handle user profile updates:
      - Validates form data using `userProfileUpdateSchema`.
      - Sends a PATCH request to update the user profile.
      - Handles response and updates session data.
      - Revalidates relevant cache tags and redirects to the updated profile page.
    
    These changes introduce a schema for validating user profile updates and an action to handle the profile update process, improving data integrity and user experience.

--- client/src/helpers/zod-schemas.ts
+++ client/src/helpers/zod-schemas.ts
@@ -1,5 +1,6 @@
 import { z } from "zod"
 
+// AUTH
 const authLoginSchema = z.object({
                                      username: z.string({ message: "Username is required" }).trim().min(3,
                                                                                                         "Username must be at least 3 characters"),
@@ -44,4 +45,56 @@ const authRegisterSchema = z
         }
     })
 
-export { authLoginSchema, authRegisterSchema }
+// USER
+const userProfileUpdateSchema = z
+    .object({
+                username: z
+                    .string({ message: "Username is required" })
+                    .trim()
+                    .min(3, "Username must be at least 3 characters")
+                    .max(50, "Username must be at most 50 characters")
+                    .regex(/^[a-z]*$/, "Username must contain only lowercase letters")
+                    .nullish()
+                    .or(z.literal("")),
+                displayName: z
+                    .string({ message: "Display Name is required" })
+                    .trim()
+                    .min(3, "Display Name must be at least 3" + " characters")
+                    .max(50, "Display Name must be at most 50 characters")
+                    .regex(/^[a-zA-Z\s]*$/, "Display Name must contain only letters and spaces")
+                    .nullish()
+                    .or(z.literal("")),
+                email: z.string({ message: "Email is required" }).trim().email().nullish().or(z.literal("")),
+                currentPassword: z.string({ message: "Current password is required" }),
+                newPassword: z
+                    .string()
+                    .min(8, "Password must be at least 8 characters")
+                    .max(100, "Password must be at most 100 characters")
+                    .regex(
+                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$/,
+                        "Password must contain at least one digit, one lowercase letter, " +
+                            "one uppercase letter, one special character and no whitespace.",
+                    )
+                    .nullish()
+                    .or(z.literal("")),
+                confirmNewPassword: z.string().optional().or(z.literal("")),
+            })
+    .superRefine(({ newPassword, confirmNewPassword }, ctx) => {
+        if (newPassword && newPassword !== confirmNewPassword) {
+            ctx.addIssue({
+                             code: "custom",
+                             message: "Passwords do not match",
+                             path: [ "confirmNewPassword" ],
+                         })
+        }
+    }).transform(data => {
+        if (data.username === "") delete data.username
+        if (data.displayName === "") delete data.displayName
+        if (data.email === "") delete data.email
+        if (data.newPassword === "") delete data.newPassword
+        if (data.confirmNewPassword) delete data.confirmNewPassword
+        
+        return data
+    })
+
+export { authLoginSchema, authRegisterSchema, userProfileUpdateSchema }
