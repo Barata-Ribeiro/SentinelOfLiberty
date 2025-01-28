0695b932713ace495c4f4e4cef3cb9c90eac837b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-01-28 06:45:35 -0300

    feat(shared-components): add FormTextarea component and update FormButton styles
    - Created `FormTextarea` component to provide a styled textarea input with a floating label.
      - Utilizes `@headlessui/react` components for accessibility and styling.
      - Supports all standard textarea attributes and a custom label.
    - Updated `FormButton` component styles for better consistency and readability.
      - Reordered Tailwind CSS classes for improved maintainability.
    
    These changes introduce a reusable textarea component and enhance the button component's styling, improving the overall UI consistency and developer experience.

--- client/src/components/shared/form-button.tsx
+++ client/src/components/shared/form-button.tsx
@@ -9,7 +9,7 @@ interface FormButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function FormButton({ children, className, ...props }: Readonly<FormButtonProps>) {
-    const defaultStyles = tw`inline-flex items-center justify-center gap-x-2 rounded-md bg-marigold-600 px-3.5 py-2.5 text-center text-sm font-semibold text-marigold-50 shadow-xs transition duration-200 hover:bg-marigold-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-marigold-600 active:bg-marigold-700 disabled:pointer-events-none disabled:opacity-50`
+    const defaultStyles = tw`bg-marigold-600 text-marigold-50 hover:bg-marigold-500 focus-visible:outline-marigold-600 active:bg-marigold-700 inline-flex items-center justify-center gap-x-2 rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-xs transition duration-200 focus-visible:outline-2 focus-visible:outline-offset-2 disabled:pointer-events-none disabled:opacity-50`
     const styles = twMerge(defaultStyles, className)
     return (
         <Button className={ styles } type="submit" { ...props }>
