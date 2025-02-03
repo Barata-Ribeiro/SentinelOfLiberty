e99da708a321fa840d161f6987d70997d5881a8d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-03 10:42:35 -0300

    feat(suggestions): add article suggestion card and skeleton components
    - Created `ArticleSuggestionCard` component to display individual article suggestions with media, title, and content.
    - Implemented `ArticleSuggestionCardSkeleton` component to show a loading state for article suggestions.
    - Integrated `Image` and `Link` components from `next/image` and `next/link` for better performance and navigation.
    - Added `react-icons/fa6` for a placeholder image in the skeleton component.
    
    These changes enhance the user experience by providing a structured and visually appealing way to display article suggestions and a skeleton for loading states.

---
+++ client/src/components/layout/skeletons/article-suggestion-card-skeleton.tsx
@@ -0,0 +1,23 @@
+import { FaImage } from "react-icons/fa6"
+
+export default function ArticleSuggestionCardSkeleton() {
+    return (
+        <output className="mx-auto mb-4 max-w-2xl rounded-md border border-stone-200 p-4 dark:border-transparent dark:bg-stone-900">
+            <div className="animate-pulse space-y-4 md:flex md:items-center md:space-y-0 md:space-x-4 rtl:space-x-reverse">
+                <div className="flex h-40 w-full items-center justify-center rounded-sm bg-stone-300 md:w-96 dark:bg-stone-700">
+                    <FaImage aria-hidden="true" className="size-10 text-stone-100 dark:text-stone-600" />
+                </div>
+
+                <div className="w-full">
+                    <div className="mb-4 h-4 max-w-48 rounded-full bg-stone-400 dark:bg-stone-800"></div>
+                    <div className="mb-2.5 h-2 rounded-full bg-stone-200 dark:bg-stone-700"></div>
+                    <div className="mb-2.5 h-2 rounded-full bg-stone-200 dark:bg-stone-700"></div>
+                    <div className="mb-2.5 h-2 rounded-full bg-stone-200 dark:bg-stone-700"></div>
+                    <div className="mb-2.5 h-2 rounded-full bg-stone-200 dark:bg-stone-700"></div>
+                    <div className="h-2 max-w-30 rounded-full bg-stone-200 sm:max-w-60 dark:bg-stone-700"></div>
+                </div>
+                <span className="sr-only">Loading...</span>
+            </div>
+        </output>
+    )
+}
---
+++ client/src/components/suggestions/article-suggestion-card.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { State }      from "@/@types/application"
+import { Suggestion } from "@/@types/suggestions"
+import Image          from "next/image"
+import Link           from "next/link"
+import { use }        from "react"
+
+interface ArticleSuggestionCardProps {
+    suggestionPromise: Promise<State> | undefined
+}
+
+export default function ArticleSuggestionCard({ suggestionPromise }: ArticleSuggestionCardProps) {
+    if (!suggestionPromise) return null
+    const suggestionState = use(suggestionPromise)
+    
+    const suggestion = suggestionState.response?.data as Suggestion
+    
+    return (
+        <section
+            className="group mx-auto mb-4 max-w-2xl rounded-md border border-stone-200 p-4 transition-colors duration-200 ease-in-out hover:bg-stone-100"
+            aria-labelledby="suggestion-title"
+            aria-describedby="suggestion-content">
+            <Link
+                href={ suggestion.sourceUrl }
+                aria-label={ `Read the full article: ${ suggestion.title }` }
+                title={ `Read the full article: ${ suggestion.title }` }
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+                className="grid grid-cols-1 content-center gap-4 md:grid-cols-[1fr_auto]">
+                <div className="relative h-72 w-full md:size-40">
+                    <Image
+                        src={ suggestion.mediaUrl }
+                        alt="Preview"
+                        quality={ 50 }
+                        fill
+                        className="aspect-square rounded-md object-cover"
+                    />
+                </div>
+
+                <div className="grid gap-y-2">
+                    <h2
+                        id="suggestion-title"
+                        className="text-shadow-900 text-xl font-semibold tracking-tight text-pretty">
+                        { suggestion.title }
+                    </h2>
+                    <p id="suggestion-content" className="text-shadow-600 prose">
+                        { suggestion.content }
+                    </p>
+                </div>
+            </Link>
+        </section>
+    )
+}
