c8965e783824cdbe135469dc328af02bf3763725
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-04 15:05:54 -0300

    feat(notifications): add action to change notification status by ID
    - Created `patchChangeNotifStatusById` function to update the status of a notification by its ID.
    - Defined `PatchChangeNotifStatusById` interface for function parameters.
    - Utilized `auth` to get the current session and access token.
    - Constructed the request URL using `changeNotificationStatusUrl`.
    - Implemented PATCH request with appropriate headers.
    - Handled response and errors, returning a structured response or error details.
    
    These changes introduce a new action to update the status of notifications, enhancing the notifications management functionality.

---
+++ client/src/actions/notifications/patch-change-notif-status-by-id.ts
@@ -0,0 +1,42 @@
+"use server"
+
+import { ProblemDetails, RestResponse } from "@/@types/application"
+import ResponseError                    from "@/actions/application/response-error"
+import { changeNotificationStatusUrl }  from "@/utils/routes"
+import { auth }                         from "auth"
+
+interface PatchChangeNotifStatusById {
+    id: number
+    status: boolean
+}
+
+export default async function patchChangeNotifStatusById({ id, status }: PatchChangeNotifStatusById) {
+    const session = await auth()
+    
+    try {
+        const URL = changeNotificationStatusUrl(id, status)
+        
+        const response = await fetch(URL, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ session?.accessToken }`,
+            },
+        })
+        
+        const json = await response.json()
+        
+        if (!response.ok) {
+            const problemDetails = json as ProblemDetails
+            return ResponseError(problemDetails)
+        }
+        
+        return {
+            ok: true,
+            error: null,
+            response: json as RestResponse,
+        }
+    } catch (error) {
+        return ResponseError(error)
+    }
+}
