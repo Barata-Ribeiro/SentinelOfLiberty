b5083b0662aa852f3c1bbb359dca12f1ad64d548
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-07 09:49:15 -0300

    feat(dashboard): enhance DashboardHomePage with latest written article display
    - Added `DashboardLatestWrittenArticle` component to display the latest written article on the dashboard.
    - Introduced `NoWrittenArticleMessage` component to show a message when no articles are written.
    - Updated the layout to include the latest written article section with a title and conditional rendering.
    - Imported necessary components and modules for the new functionality.

--- client/src/app/dashboard/[username]/page.tsx
+++ client/src/app/dashboard/[username]/page.tsx
@@ -1,9 +1,11 @@
-import { Dashboard, Profile }    from "@/@types/user"
-import getOwnDashboard           from "@/actions/users/get-own-dashboard"
-import getOwnProfile             from "@/actions/users/get-own-profile"
-import DashboardSiteInteractions from "@/components/dashboard/dashboard-site-interactions"
-import DashboardUserProfile      from "@/components/dashboard/dashboard-user-profile"
-import { notFound }              from "next/navigation"
+import { Dashboard, Profile }        from "@/@types/user"
+import getOwnDashboard               from "@/actions/users/get-own-dashboard"
+import getOwnProfile                 from "@/actions/users/get-own-profile"
+import DashboardLatestWrittenArticle from "@/components/dashboard/dashboard-latest-written-article"
+import DashboardSiteInteractions     from "@/components/dashboard/dashboard-site-interactions"
+import DashboardUserProfile          from "@/components/dashboard/dashboard-user-profile"
+import Link                          from "next/link"
+import { notFound }                  from "next/navigation"
 
 interface DashboardHomePageProps {
     params: Promise<{ username: string }>
@@ -15,6 +17,26 @@ export async function generateMetadata({ params }: Readonly<DashboardHomePagePro
         title: `${ username }`,
         description: "This is the dashboard home page",
     }
+}
+
+function NoWrittenArticleMessage(props: { profile: Profile }) {
+    return (
+        <p className="text-shadow-600 text-center">
+            You have not written any articles.{ " " }
+            { props.profile.role === "ADMIN" ? (
+                <>
+                    Write one now{ " " }
+                    <Link
+                        href="/articles/write"
+                        className="text-marigold-500 hover:text-marigold-600 active:text-marigold-700 hover:underline">
+                        here!
+                    </Link>
+                </>
+            ) : (
+                  "Wait for one of our creators to write one."
+              ) }
+        </p>
+    )
 }
 
 export default async function DashboardHomePage({ params }: Readonly<DashboardHomePageProps>) {
@@ -36,6 +58,8 @@ export default async function DashboardHomePage({ params }: Readonly<DashboardHo
     console.log("dashboard: ", dashboard)
     console.groupEnd()
     
+    const latestWrittenArticle = dashboard.latestWrittenArticle
+    
     return (
         <div className="my-8 grid grid-cols-1 gap-2 sm:gap-4 md:grid-cols-[33%_66%] lg:gap-8">
             <aside className="grid gap-y-2 *:rounded-md *:border-2 *:border-stone-200 *:p-4 sm:gap-y-4 lg:gap-y-8">
@@ -44,7 +68,13 @@ export default async function DashboardHomePage({ params }: Readonly<DashboardHo
                 <DashboardSiteInteractions dashboard={ dashboard } />
             </aside>
 
-            <main className="rounded-md border-2 border-stone-200 p-4"></main>
+            <main className="rounded-md border-2 border-stone-200 p-4">
+                <div className="grid gap-y-4">
+                    <h2 className="text-shadow-900 border-b-2 font-medium">Latest Written Article</h2>
+                    { !latestWrittenArticle && <NoWrittenArticleMessage profile={ profile } /> }
+                    { latestWrittenArticle && <DashboardLatestWrittenArticle article={ latestWrittenArticle } /> }
+                </div>
+            </main>
         </div>
     )
 }
