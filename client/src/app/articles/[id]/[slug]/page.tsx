dab9c3a55548cd510b17650eedd74a431172f62b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-10 10:36:38 -0300

    feat(article-page): enhance article page with comments and new comment form
    - Added imports for `getCommentTree`, `MainArticleCommentTree`, `NewCommentForm`, and `CommentSkeleton`.
    - Fixed typo in `latestArticlesPromise`.
    - Added `id` attributes to various sections for better accessibility and identification.
    - Implemented `commentTreePromise` to fetch the comment tree for the article.
    - Added `NewCommentForm` section to allow users to add new comments.
    - Added `MainArticleCommentTree` section to display the comment tree with a suspense fallback.
    
    These changes enhance the article page by adding functionality for displaying and adding comments, improving user interaction and engagement.

--- client/src/app/articles/[id]/[slug]/page.tsx
+++ client/src/app/articles/[id]/[slug]/page.tsx
@@ -1,12 +1,16 @@
 import { Article }                     from "@/@types/articles"
 import getArticleById                  from "@/actions/articles/get-article-by-id"
 import getLatestPublicArticles         from "@/actions/articles/get-latest-public-articles"
+import getCommentTree                  from "@/actions/comments/get-comment-tree"
 import LatestArticlesListing           from "@/components/articles/latest-articles-listing"
 import MainArticleAuthor               from "@/components/articles/main-article-author"
+import MainArticleCommentTree          from "@/components/articles/main-article-comment-tree"
 import MainArticleFooter               from "@/components/articles/main-article-footer"
 import MainArticleHeader               from "@/components/articles/main-article-header"
 import MainArticleReferences           from "@/components/articles/main-article-references"
+import NewCommentForm                  from "@/components/forms/new-comment-form"
 import MemoizedContent                 from "@/components/helpers/memoized-content"
+import CommentSkeleton                 from "@/components/layout/skeletons/comment-skeleton"
 import { auth }                        from "auth"
 import { Metadata, ResolvingMetadata } from "next"
 import { headers }                     from "next/headers"
@@ -47,7 +51,7 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
     const { id, slug } = await params
     if (!id || !slug) return notFound()
     
-    const latestAriclesPromise = getLatestPublicArticles()
+    const latestArticlesPromise = getLatestPublicArticles()
     
     const [ headersList, session, articleState ] = await Promise
         .all([ headers(), auth(), getArticleById({ id: parseInt(id) }) ])
@@ -55,10 +59,13 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
     
     const pathname = headersList.get("x-current-path")
     const article = articleState.response?.data as Article
+    if (article.slug !== slug) return notFound()
+    
+    const commentTreePromise = getCommentTree({ articleId: article.id })
     
     return (
         <div className="grid grid-cols-1 gap-4">
-            <article className="container space-y-6">
+            <article id="article" className="container space-y-6">
                 <MainArticleHeader article={ article } />
 
                 <main className="grid grid-cols-1 gap-4 md:grid-cols-[auto_1fr] md:gap-8">
@@ -67,34 +74,45 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
                         <MainArticleReferences article={ article } />
                     </aside>
 
-                    <div className="w-full !max-w-4xl">
+                    <div id="article-content" className="w-full !max-w-4xl">
                         <MemoizedContent html={ article.content } />
                         <MainArticleFooter article={ article } pathname={ pathname } />
                     </div>
                 </main>
             </article>
 
-            <section className="bg-stone-200">
+            <section id="latest-articles-section" className="bg-stone-200">
                 <div className="container grid grid-cols-1 gap-4 md:grid-cols-[auto_1fr] md:gap-8">
                     <div className="mt-8 min-w-0 space-y-2 sm:min-w-[19rem]">
                         <h3 className="text-shadow-900 text-4xl font-semibold text-balance">Latest Articles</h3>
                         <Link
                             href="/articles"
-                            className="text-marigold-600 hover:text-marigold-700 active:text-marigold-800 tracking-wide text-sm underline">
+                            className="text-marigold-600 hover:text-marigold-700 active:text-marigold-800 text-sm tracking-wide underline">
                             View all articles
                         </Link>
                     </div>
 
                     <div className="grid max-w-4xl grid-cols-1 justify-items-center gap-4 md:grid-cols-2 lg:grid-cols-3">
                         <Suspense fallback={ <p>Loading...</p> }>
-                            <LatestArticlesListing articlesState={ latestAriclesPromise } />
+                            <LatestArticlesListing articlesState={ latestArticlesPromise } />
                         </Suspense>
                     </div>
                 </div>
             </section>
             
-            {/*TODO: Implement new comment form*/ }
-            {/*TODO: Implement article comments when entering the comments range*/ }
+            <section id="new-comment-form" className="container" aria-labelledby="new-comment-section-title">
+                <h2 id="new-comment-section-title"
+                    className="text-shadow-900 mb-6 border-b border-stone-200 pb-3 text-2xl font-bold">New Comment</h2>
+                <NewCommentForm />
+            </section>
+            
+            <section id="article-comments" className="container" aria-labelledby="comments-section-title">
+                <h2 id="comments-section-title"
+                    className="text-shadow-900 mb-6 border-b border-stone-200 pb-3 text-2xl font-bold">Comments</h2>
+                <Suspense fallback={ <CommentSkeleton /> }>
+                    <MainArticleCommentTree commentTreePromise={ commentTreePromise } />
+                </Suspense>
+            </section>
         </div>
     )
 }
