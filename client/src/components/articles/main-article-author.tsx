9205d2967f5e44e6023688dc6befa9034a2e9326
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-12 10:06:17 -0300

    fix(author): correct link URL and improve code formatting in main article author component
    - Fixed the link URL to use `article.author.username` for both authenticated and unauthenticated users.
    - Improved code formatting for better readability and consistency.

--- client/src/components/articles/main-article-author.tsx
+++ client/src/components/articles/main-article-author.tsx
@@ -1,6 +1,6 @@
-import { Article } from "@/@types/articles"
-import { Session } from "next-auth"
-import Link from "next/link"
+import { Article }               from "@/@types/articles"
+import { Session }               from "next-auth"
+import Link                      from "next/link"
 import { FaCircleCheck, FaUser } from "react-icons/fa6"
 
 interface MainArticleAuthorProps {
@@ -14,19 +14,21 @@ export default function MainArticleAuthor({ session, article }: Readonly<MainArt
             <h3 className="text-shadow-900 text-xl">Author</h3>
             <div className="flex w-max items-center justify-between p-4">
                 <Link
-                    href={session ? `/dashboard/${article.author.id}` : `/profile/${article.author.username}`}
-                    aria-label={session ? "You're the author" : `Author ${article.author.username}`}
-                    title={session ? "You're the author" : `Author ${article.author.username}`}
+                    href={ session
+                           ? `/dashboard/${ article.author.username }`
+                           : `/profile/${ article.author.username }` }
+                    aria-label={ session ? "You're the author" : `Author ${ article.author.username }` }
+                    title={ session ? "You're the author" : `Author ${ article.author.username }` }
                     className="text-shadow-400 hover:text-shadow-500 inline-flex min-w-0 items-center gap-x-2 text-sm hover:underline max-sm:max-w-[2rem]">
                     <span className="bg-marigold-100 shrink-0 rounded-full p-2">
                         <FaUser aria-hidden="true" className="size-4" />
                     </span>
-                    {article.author.username}
-                    {article.author.isVerified && (
+                    { article.author.username }
+                    { article.author.isVerified && (
                         <span className="text-marigold-500" aria-label="Verified author" title="Verified author">
                             <FaCircleCheck aria-hidden="true" className="size-4" />
                         </span>
-                    )}
+                    ) }
                 </Link>
             </div>
         </div>
