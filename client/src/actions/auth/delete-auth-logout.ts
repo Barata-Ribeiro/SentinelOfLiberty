67fa750389efa6f3a401192fa5c2d0990a88e590
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-16 07:10:47 -0300

    feat(auth): implement deleteAuthLogout function for user logout
    - Created `deleteAuthLogout` function to handle user logout by sending a DELETE request to the authentication logout endpoint.
    - Utilized `getTokenAndExpiration` to retrieve the refresh token and included it in the request headers.
    - Handled the response by checking for errors and returning appropriate error messages using `ResponseError`.
    - Called `signOut` from `auth` to sign out the user without redirecting.
    - Cleared all cookies using the `cookies` utility from `next/headers`.

---
+++ client/src/actions/auth/delete-auth-logout.ts
@@ -0,0 +1,40 @@
+"use server"
+
+import ResponseError             from "@/actions/application/response-error"
+import { logoutAuthUrl }         from "@/utils/routes"
+import { getTokenAndExpiration } from "@/utils/server-utilities"
+import { signOut }               from "auth"
+import { cookies }               from "next/headers"
+
+export default async function deleteAuthLogout() {
+    const cookieStore = await cookies()
+    const tokenAndExpiration = await getTokenAndExpiration()
+    
+    try {
+        if (!tokenAndExpiration) return ResponseError("Something went wrong...")
+        
+        const URL = logoutAuthUrl()
+        
+        const response = await fetch(URL, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Refresh-Token": tokenAndExpiration.token,
+            },
+        })
+        
+        await signOut({ redirect: false })
+        
+        if (cookieStore) {
+            cookieStore.getAll().forEach(cookie => cookieStore.delete(cookie.name))
+        }
+        
+        return {
+            ok: response.status === 204,
+            error: null,
+            response: "You have successfully logged out.",
+        }
+    } catch (error) {
+        return ResponseError(error)
+    }
+}
