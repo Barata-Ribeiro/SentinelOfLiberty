4598a0207a20644ad467a5ef3c75807470a57dd4
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-17 06:51:14 -0300

    refactor(article-summary-card): improve layout and structure
    - Removed `RoleBadge` import as it is no longer used in the component.
    - Reorganized the layout to better align the author information and article metadata.
    - Moved the article metadata (comments count and creation date) to be displayed alongside the author information.
    - Removed the unnecessary ring element from the image container.

--- client/src/components/articles/article-summary-card.tsx
+++ client/src/components/articles/article-summary-card.tsx
@@ -1,6 +1,5 @@
 import { ArticleSummary }            from "@/@types/articles"
 import Avatar                        from "@/components/shared/avatar"
-import RoleBadge                     from "@/components/shared/role-badge"
 import Image                         from "next/image"
 import Link                          from "next/link"
 import { FaCircleCheck, FaComments } from "react-icons/fa6"
@@ -20,23 +19,8 @@ export default function ArticleSummaryCard({ article }: Readonly<ArticleSummaryC
                     sizes="(min-width: 808px) 50vw, 100vw"
                     fill
                 />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-stone-900/10 ring-inset" />
             </div>
             <div className="max-w-xl">
-                <div className="mt-8 flex items-center gap-x-4 text-xs">
-                    <time dateTime={ article.createdAt.toString() } className="text-shadow-500">
-                        { new Date(article.createdAt).toLocaleDateString("en-US", {
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                        }) }
-                    </time>
-
-                    <span className="text-shadow-500 flex items-center gap-x-1">
-                        <FaComments aria-hidden="true" className="size-5" />
-                        <span>({ article.commentsCount })</span>
-                    </span>
-                </div>
                 <div className="group relative">
                     <div className="grid">
                         <h3 className="text-shadow-900 group-hover:text-shadow-600 mt-3 text-lg font-semibold">
@@ -52,19 +36,36 @@ export default function ArticleSummaryCard({ article }: Readonly<ArticleSummaryC
 
                     <p className="text-shadow-600 mt-5 line-clamp-3 text-sm leading-6">{ article.summary }</p>
                 </div>
-                <div className="relative mt-8 flex items-center gap-x-4">
-                    <Avatar name={ article.author.username } src={ article.author.avatarUrl } size={ 36 } />
-                    <Link href={ `/profile/${ article.author.username }` } className="w-max text-sm">
-                        <p className="text-shadow-900 flex items-center gap-x-1 font-semibold">
+
+                <div className="flex items-center justify-between">
+                    <div className="relative mt-8 flex items-center gap-x-4">
+                        <Avatar name={ article.author.username } src={ article.author.avatarUrl } size={ 36 } />
+                        <Link
+                            href={ `/profile/${ article.author.username }` }
+                            className="text-shadow-900 inline-flex w-max items-center gap-x-1 text-sm font-semibold">
                             { article.author.username }
                             { article.author.isVerified && (
                                 <span className="text-marigold-500" title="Verified author">
                                     <FaCircleCheck aria-hidden="true" className="size-4" />
                                 </span>
                             ) }
-                        </p>
-                        <RoleBadge role={ article.author.role } />
-                    </Link>
+                        </Link>
+                    </div>
+
+                    <div className="mt-8 flex items-center gap-x-4 text-xs">
+                        <span className="text-shadow-500 flex items-center gap-x-1">
+                            <FaComments aria-hidden="true" className="size-5" />
+                            <span>({ article.commentsCount })</span>
+                        </span>
+
+                        <time dateTime={ article.createdAt.toString() } className="text-shadow-500">
+                            { new Date(article.createdAt).toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            }) }
+                        </time>
+                    </div>
                 </div>
             </div>
         </article>
