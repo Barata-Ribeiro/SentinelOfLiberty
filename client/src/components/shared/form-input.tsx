d48397e75f9c648d15e6531575631ee7e8e5a596
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-20 06:51:38 -0300

    refactor(form-components): altered current form components with new style
    - Updated `FormTextarea` component:
      - Added optional `description` prop to display additional information below the textarea.
      - Improved styling and structure for better user experience.
      - Included `LuInfo` icon for descriptions.
    
    - Updated `FormInput` component:
      - Added optional `icon` and `iconPlacement` props to support icons within the input field.
      - Improved styling and structure for better user experience.
      - Included `LuInfo` icon for descriptions.
      - Added validation to ensure `icon` and `iconPlacement` are used together.

--- client/src/components/shared/form-input.tsx
+++ client/src/components/shared/form-input.tsx
@@ -1,58 +1,60 @@
-import tw                                             from "@/utils/tw"
-import { Description, Field, Input, Label }           from "@headlessui/react"
-import { ChangeEvent, InputHTMLAttributes, useState } from "react"
-import { twMerge }                                    from "tailwind-merge"
+import tw                                   from "@/utils/tw"
+import { Description, Field, Input, Label } from "@headlessui/react"
+import { InputHTMLAttributes }              from "react"
+import { IconType }                         from "react-icons"
+import { LuInfo }                           from "react-icons/lu"
+import { twMerge }                          from "tailwind-merge"
 
 interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
-    disabledField?: boolean
     label: string
     description?: string
+    icon?: IconType
+    iconPlacement?: "start" | "end"
 }
 
 export default function FormInput({
                                       label,
                                       description,
-                                      disabledField,
-                                      onChange,
-                                      defaultValue,
-                                      value,
+                                      icon: Icon,
+                                      iconPlacement,
                                       className,
                                       ...props
                                   }: Readonly<FormInputProps>) {
-    const [ hasValue, setHasValue ] = useState(!!value || !!defaultValue)
-    
-    function handleChange(e: ChangeEvent<HTMLInputElement>) {
-        setHasValue(!!e.target.value)
-        onChange?.(e)
+    if ((Icon && !iconPlacement) || (iconPlacement && !Icon)) {
+        throw new Error("Icon and iconPlacement must be used together.")
     }
     
-    const labelStyle = tw`text-shadow-400 peer-focus:text-shadow-400 absolute left-2.5 origin-left transform cursor-text bg-white px-1 text-sm transition-all peer-focus:-top-2 peer-focus:scale-90 peer-focus:text-xs data-disabled:pointer-events-none data-disabled:opacity-50 data-disabled:select-none`
-    
-    const inputStyle = tw`peer text-shadow-700 placeholder:text-shadow-400 w-full rounded-md border border-stone-200 bg-transparent px-3 py-2 text-sm shadow-xs transition duration-300 hover:border-stone-300 focus:border-stone-400 focus:ring-1 focus:shadow-sm focus:ring-stone-400/50 focus:outline-hidden data-disabled:pointer-events-none data-disabled:bg-stone-100`
+    const inputDefaultStyles = tw`peer text-shadow-800 placeholder:text-shadow-300 w-full rounded-md border border-stone-200 bg-transparent px-2.5 py-2 text-sm ring shadow-sm ring-transparent transition-all duration-300 ease-in outline-none hover:border-stone-800 hover:ring-stone-300 focus:border-stone-800 focus:ring-stone-300 focus:outline-none disabled:pointer-events-none disabled:opacity-50 aria-disabled:cursor-not-allowed data-[icon-placement=end]:pe-9 data-[icon-placement=start]:ps-9 dark:text-shadow-50`
     
     return (
-        <div className="w-full min-w-[200px]">
-            <Field className="relative" disabled={ disabledField }>
+        <Field className="w-full min-w-[200px] space-y-2" disabled={ props.disabled }>
+            <Label className="text-shadow-800 text-sm font-semibold antialiased dark:text-shadow-50">
+                { label }
+            </Label>
+            
+            <div className="relative w-full">
                 <Input
-                    className={ twMerge(className, inputStyle) }
-                    value={ value }
-                    defaultValue={ defaultValue }
-                    onChange={ handleChange }
+                    className={ twMerge(inputDefaultStyles, className) }
+                    data-icon-placement={ iconPlacement }
                     { ...props }
                 />
-                <Label
-                    className={ twMerge(
-                        labelStyle,
-                        hasValue ? "text-shadow-400 -top-2 scale-90 text-xs" : "top-2.5 scale-100 text-sm",
-                    ) }>
-                    { label }
-                </Label>
-                { description && (
-                    <Description className="text-shadow-300 mt-1 px-1.5 text-xs data-disabled:pointer-events-none data-disabled:opacity-50">
+                { Icon && (
+                    <span
+                        className="text-shadow-400 peer-focus:text-shadow-800 pointer-events-none absolute top-1/2 h-5 w-5 -translate-y-1/2 overflow-hidden transition-all duration-300 ease-in data-[placement=end]:right-2.5 data-[placement=start]:left-2.5 dark:peer-hover:text-shadow-50 dark:peer-focus:text-shadow-50"
+                        data-placement={ iconPlacement }>
+                        <Icon aria-hidden="true" className="h-full w-full" />
+                    </span>
+                ) }
+            </div>
+            
+            { description && (
+                <div className="text-shadow-600 flex gap-1.5">
+                    <LuInfo aria-hidden="true" className="size-3.5 shrink-0 translate-y-[3px] stroke-2" />
+                    <Description as="small" className="text-sm text-current antialiased">
                         { description }
                     </Description>
-                ) }
-            </Field>
-        </div>
+                </div>
+            ) }
+        </Field>
     )
 }
--- client/src/components/shared/form-textarea.tsx
+++ client/src/components/shared/form-textarea.tsx
@@ -1,20 +1,31 @@
-import { Field, Label, Textarea } from "@headlessui/react"
-import { TextareaHTMLAttributes } from "react"
+import { Description, Field, Label, Textarea } from "@headlessui/react"
+import { TextareaHTMLAttributes }              from "react"
+import { LuInfo }                              from "react-icons/lu"
 
 interface FormTextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
     label: string
+    description?: string
 }
 
-export default function FormTextarea({ label, ...props }: FormTextareaProps) {
+export default function FormTextarea({ label, description, ...props }: FormTextareaProps) {
     return (
-        <Field className="relative w-full min-w-[200px]" disabled={ props.disabled }>
-            <Textarea
-                { ...props }
-                className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-stone-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-shadow-700 outline-0 transition-all placeholder-shown:border placeholder-shown:border-stone-200 placeholder-shown:border-t-stone-200 focus:border-2 focus:border-stone-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-stone-50"
-                placeholder=" "></Textarea>
-            <Label className="before:content[' '] after:content[' '] text-shadow-400 peer-placeholder-shown:text-shadow-500 peer-disabled:peer-placeholder-shown:text-shadow-500 peer-focus:text-shadow-900 pointer-events-none absolute -top-1.5 left-0 flex h-full w-full text-xs leading-tight font-normal transition-all select-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-focus:text-[11px] peer-focus:leading-tight peer-disabled:text-transparent before:pointer-events-none before:mt-[6.2px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-stone-200 before:transition-all peer-placeholder-shown:before:border-transparent peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-stone-900 peer-disabled:before:border-transparent after:pointer-events-none after:mt-[6.2px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-stone-200 after:transition-all peer-placeholder-shown:after:border-transparent peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-stone-900 peer-disabled:after:border-transparent">
+        <Field className="relative w-full min-w-[200px] space-y-2" disabled={ props.disabled }>
+            <Label className="text-shadow-800 dark:text-shadow-50 font-sans text-sm font-semibold antialiased">
                 { label }
             </Label>
+
+            <Textarea
+                { ...props }
+                className="peer text-shadow-800 placeholder:text-shadow-300 dark:text-shadow-50 block w-full resize-none rounded-md border border-stone-200 bg-transparent p-2.5 text-sm leading-none ring shadow-sm ring-transparent transition-all duration-300 ease-in outline-none hover:border-stone-800 hover:ring-stone-300 focus:border-stone-800 focus:ring-stone-300 focus:outline-none disabled:pointer-events-none disabled:opacity-50"></Textarea>
+            
+            { description && (
+                <div className="text-shadow-600 flex gap-1.5">
+                    <LuInfo aria-hidden="true" className="size-3.5 shrink-0 translate-y-[3px] stroke-2" />
+                    <Description as="small" className="text-sm text-current antialiased">
+                        { description }
+                    </Description>
+                </div>
+            ) }
         </Field>
     )
 }
