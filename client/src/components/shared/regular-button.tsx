08788d1657d68974b7f952922fce146a7a393e9d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-20 14:33:46 -0300

    refactor(buttons): enhance button components with new styles and loading state
    - Updated `LinkButton` component:
      - Added `buttonStyle` prop to support "ghost", "inverted-ghost", and "colored" styles.
      - Defined base styles and specific styles for each button type.
      - Applied the appropriate styles based on the `buttonStyle` prop.
    
    - Updated `RegularButton` component:
      - Added `buttonStyle` prop to support "ghost", "inverted-ghost", "colored", and "danger" styles.
      - Added `pending` prop to display a loading spinner and disable the button during loading state.
      - Defined base styles and specific styles for each button type.
      - Applied the appropriate styles based on the `buttonStyle` prop.
      - Included conditional rendering for the loading state.

--- client/src/components/shared/regular-button.tsx
+++ client/src/components/shared/regular-button.tsx
@@ -1,19 +1,69 @@
+import Spinner                             from "@/components/helpers/spinner"
 import tw                                  from "@/utils/tw"
 import { Button }                          from "@headlessui/react"
 import { ButtonHTMLAttributes, ReactNode } from "react"
 import { twMerge }                         from "tailwind-merge"
 
 interface RegularButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    children?: ReactNode
-    className?: string
+    children: ReactNode
+    buttonStyle: "ghost" | "inverted-ghost" | "colored" | "danger"
+    pending?: boolean
 }
 
-export default function RegularButton({ children, className, ...props }: Readonly<RegularButtonProps>) {
-    const defaultStyles = tw`bg-marigold-600 text-marigold-50 hover:bg-marigold-500 focus-visible:outline-marigold-600 active:bg-marigold-700 inline-flex cursor-pointer items-center justify-center gap-x-2 rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-xs transition duration-200 select-none focus-visible:outline-2 focus-visible:outline-offset-2`
-    const styles = twMerge(defaultStyles, className)
+export default function RegularButton({
+                                          children,
+                                          className,
+                                          buttonStyle,
+                                          pending,
+                                          ...props
+                                      }: Readonly<RegularButtonProps>) {
+    const baseStyles = tw`inline-flex cursor-pointer items-center gap-x-2 rounded-md border px-4 py-2 text-center text-sm font-medium transition-all duration-300 ease-in disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none`
+    
+    const coloredStyle = twMerge(
+        baseStyles,
+        tw`from-marigold-500 to-marigold-600 border-marigold-500 text-shadow-50 bg-gradient-to-tr shadow-sm hover:shadow-md hover:brightness-110`,
+    )
+    
+    const ghostStyle = twMerge(
+        baseStyles,
+        tw`border border-transparent bg-transparent text-stone-900 shadow-none hover:border-stone-200 hover:bg-stone-100 hover:shadow-none`,
+    )
+    
+    const invertedGhostStyle = twMerge(
+        baseStyles,
+        tw`border border-transparent bg-transparent text-stone-50 shadow-none hover:border-stone-300 hover:bg-stone-300 hover:text-stone-900 hover:shadow-none`,
+    )
+    
+    const alertStyle = twMerge(
+        baseStyles,
+        className,
+        tw`text-shadow-50 border-red-500 bg-gradient-to-tr from-red-500 to-red-700 shadow-sm hover:shadow-md hover:brightness-110`,
+    )
+    
+    let buttonFinalStyle
+    switch (buttonStyle) {
+        case "ghost":
+            buttonFinalStyle = ghostStyle
+            break
+        case "inverted-ghost":
+            buttonFinalStyle = invertedGhostStyle
+            break
+        case "danger":
+            buttonFinalStyle = alertStyle
+            break
+        default:
+            buttonFinalStyle = coloredStyle
+    }
+    
     return (
-        <Button className={ styles } type="button" { ...props }>
-            { children }
+        <Button type="button" className={ buttonFinalStyle } disabled={ props.disabled ?? pending } { ...props }>
+            { pending ? (
+                <>
+                    <Spinner /> Loading...
+                </>
+            ) : (
+                  children
+              ) }
         </Button>
     )
 }
