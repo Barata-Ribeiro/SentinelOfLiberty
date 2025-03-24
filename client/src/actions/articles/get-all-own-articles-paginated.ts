fa298646c1b1294fb5f396541b6b638cef89ae04
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-24 06:17:47 -0300

    feat(api): add `getAllOwnArticlesPaginated` function to fetch paginated articles

---
+++ client/src/actions/articles/get-all-own-articles-paginated.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import { ProblemDetails, RestResponse } from "@/@types/application"
+import ResponseError                    from "@/actions/application/response-error"
+import { auth }                         from "@/auth"
+import { getAllOwnArticlesUrl }         from "@/utils/routes"
+
+interface GetAllOwnArticlesPaginated {
+    page: number
+    perPage: number
+    direction: string
+    orderBy: string
+    search: string
+}
+
+export default async function getAllOwnArticlesPaginated({
+                                                             page,
+                                                             perPage,
+                                                             direction,
+                                                             orderBy,
+                                                             search,
+                                                         }: GetAllOwnArticlesPaginated) {
+    const session = await auth()
+    
+    try {
+        const URL = getAllOwnArticlesUrl(search, page, perPage, direction, orderBy)
+        
+        const response = await fetch(URL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ session?.accessToken }`,
+            },
+            next: { revalidate: 120, tags: [ "articles" ] },
+        })
+        
+        const json = await response.json()
+        
+        if (!response.ok) {
+            const problemDetails = json as ProblemDetails
+            return ResponseError(problemDetails)
+        }
+        
+        return {
+            ok: true,
+            error: null,
+            response: json as RestResponse,
+        }
+    } catch (error) {
+        return ResponseError(error)
+    }
+}
