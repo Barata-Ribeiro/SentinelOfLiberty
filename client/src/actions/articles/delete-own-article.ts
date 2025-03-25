20b7d1d99f35c6a3bacf13718b9d993990ca71c9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-25 05:31:49 -0300

    feat(api): add function to delete own articles

---
+++ client/src/actions/articles/delete-own-article.ts
@@ -0,0 +1,46 @@
+"use server"
+
+import { ProblemDetails }   from "@/@types/application"
+import ResponseError        from "@/actions/application/response-error"
+import { deleteArticleUrl } from "@/utils/routes"
+import { auth }             from "auth"
+import { revalidateTag }    from "next/cache"
+
+interface DeleteOwnArticle {
+    id: number
+}
+
+export default async function deleteOwnArticle({ id }: DeleteOwnArticle) {
+    const session = await auth()
+    try {
+        const URL = deleteArticleUrl(id)
+        
+        const response = await fetch(URL, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ session?.accessToken }`,
+            },
+        })
+        
+        const json = await response.json()
+        
+        if (!response.ok) {
+            const problemDetails = json as ProblemDetails
+            return ResponseError(problemDetails)
+        }
+        
+        revalidateTag("articles")
+        revalidateTag("categories")
+        revalidateTag("dashboard")
+        revalidateTag("profile")
+        
+        return {
+            ok: response.status === 204,
+            error: null,
+            response: "You have successfully deleted the article",
+        }
+    } catch (error) {
+        return ResponseError(error)
+    }
+}
