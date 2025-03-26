27d37188a98cba35e164580841026cb61c70ea82
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-26 05:49:09 -0300

    refactor(form-textarea): update class structure for Textarea and description elements

--- client/src/components/shared/form-textarea.tsx
+++ client/src/components/shared/form-textarea.tsx
@@ -16,15 +16,15 @@ export default function FormTextarea({ label, description, ...props }: Readonly<
 
             <Textarea
                 { ...props }
-                className="peer text-shadow-800 placeholder:text-shadow-300 dark:text-shadow-50 block w-full resize-none rounded-md border border-stone-200 bg-transparent p-2.5 text-sm leading-none ring shadow-sm ring-transparent transition-all duration-300 ease-in outline-none hover:border-stone-800 hover:ring-stone-300 focus:border-stone-800 focus:ring-stone-300 focus:outline-none disabled:pointer-events-none disabled:opacity-50"></Textarea>
+                className="peer text-shadow-800 placeholder:text-shadow-300 dark:text-shadow-50 block w-full resize-none rounded-md border border-stone-200 bg-transparent p-2.5 text-sm leading-none shadow-sm ring ring-transparent transition-all duration-300 ease-in outline-none hover:border-stone-800 hover:ring-stone-300 focus:border-stone-800 focus:ring-stone-300 focus:outline-none disabled:pointer-events-none disabled:opacity-50"></Textarea>
             
             { description && (
-                <div className="text-shadow-600 flex gap-1.5">
+                <p className="text-shadow-600 flex gap-1.5">
                     <LuInfo aria-hidden="true" className="size-3.5 shrink-0 translate-y-[3px] stroke-2" />
                     <Description as="small" className="text-sm text-current antialiased">
                         { description }
                     </Description>
-                </div>
+                </p>
             ) }
         </Field>
     )
