f87be26c8de0a04c25b6d02af26aad82abb9b41a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-28 06:27:12 -0300

    fix(principal): rename `StomPrincipal` to `StompPrincipal`

--- server/src/main/java/com/barataribeiro/sentinelofliberty/config/StompPrincipal.java
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/config/StompPrincipal.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import java.security.Principal;
 
 @RequiredArgsConstructor
-public class StomPrincipal implements Principal {
+public class StompPrincipal implements Principal {
     private final String name;
 
     @Override
