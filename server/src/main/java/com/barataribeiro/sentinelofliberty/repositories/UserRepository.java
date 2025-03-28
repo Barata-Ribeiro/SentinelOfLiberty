abf2cbc1416463103923ad3de762f50ec44e412d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-28 09:56:34 -0300

    feat(user-repository): add paginated search for users by term

--- server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/UserRepository.java
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/repositories/UserRepository.java
@@ -2,7 +2,13 @@ package com.barataribeiro.sentinelofliberty.repositories;
 
 import com.barataribeiro.sentinelofliberty.models.entities.User;
 import com.barataribeiro.sentinelofliberty.models.enums.Roles;
+import jakarta.persistence.QueryHint;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +18,22 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 
     List<User> findAllByRole(Roles role);
+
+    @Query("""
+           SELECT u FROM User u
+           WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :term, '%'))
+           OR LOWER(u.email) LIKE LOWER(CONCAT('%', :term, '%'))
+           OR LOWER(u.displayName) LIKE LOWER(CONCAT('%', :term, '%'))
+           OR LOWER(u.fullName) LIKE LOWER(CONCAT('%', :term, '%'))
+           """)
+    @QueryHints({
+            @QueryHint(name = "org.hibernate.readOnly", value = "true"),
+            @QueryHint(name = "org.hibernate.fetchSize", value = "20"),
+            @QueryHint(name = "org.hibernate.cacheable", value = "true"),
+            @QueryHint(name = "jakarta.persistence.cache.retrieveMode", value = "USE"),
+            @QueryHint(name = "jakarta.persistence.cache.storeMode", value = "USE")
+    })
+    Page<User> findAllUsersBySearchParams(@Param("term") String term, Pageable pageable);
 
     boolean existsByUsernameOrEmailAllIgnoreCase(String username, String email);
 
