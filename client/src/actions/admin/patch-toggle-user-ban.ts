9d6c825244bdd903020ba780381a7af9986052a1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-30 09:26:40 -0300

    feat(admin): implement patch function to toggle user ban status

---
+++ client/src/actions/admin/patch-toggle-user-ban.ts
@@ -0,0 +1,49 @@
+"use server"
+
+import { ProblemDetails, RestResponse } from "@/@types/application"
+import ResponseError                    from "@/actions/application/response-error"
+import { adminBanOrUnbanUserUrl }       from "@/utils/routes"
+import { auth }                         from "auth"
+import { revalidateTag }                from "next/cache"
+
+interface PatchToggleUserBan {
+    username: string
+    isBanned: boolean
+}
+
+export default async function patchToggleUserBan({ username, isBanned }: PatchToggleUserBan) {
+    const session = await auth()
+    
+    try {
+        const banOrUnban = isBanned ? "unban" : "ban"
+        const URL = adminBanOrUnbanUserUrl(username, banOrUnban)
+        
+        const response = await fetch(URL, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ session?.accessToken }`,
+            },
+            body: JSON.stringify({}),
+        })
+        
+        const json = await response.json()
+        
+        if (!response.ok) {
+            const problemDetails = json as ProblemDetails
+            return ResponseError(problemDetails)
+        }
+        
+        revalidateTag("profile")
+        revalidateTag("user-settings")
+        revalidateTag("dashboard")
+        
+        return {
+            ok: true,
+            error: null,
+            response: json as RestResponse,
+        }
+    } catch (error) {
+        return ResponseError(error)
+    }
+}
