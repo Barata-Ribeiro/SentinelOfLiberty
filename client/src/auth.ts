7e073904a30143c5683604381afa477083139cdd
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-05-12 15:36:16 -0300

    fix(auth): use another next auth import location

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -1,11 +1,11 @@
-import { RestResponse }             from "@/@types/application"
-import { LoginResponse }            from "@/@types/auth"
-import createCookie                 from "@/actions/application/create-cookie"
-import refreshAccessToken           from "@/actions/application/token-refresh"
-import { loginAuthUrl }             from "@/utils/routes"
-import { getTokenAndExpiration }    from "@/utils/server-utilities"
-import Credentials                  from "@auth/core/providers/credentials"
+import { RestResponse } from "@/@types/application"
+import { LoginResponse } from "@/@types/auth"
+import createCookie from "@/actions/application/create-cookie"
+import refreshAccessToken from "@/actions/application/token-refresh"
+import { loginAuthUrl } from "@/utils/routes"
+import { getTokenAndExpiration } from "@/utils/server-utilities"
 import NextAuth, { NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
 
 export const config = {
     pages: {
@@ -14,42 +14,42 @@ export const config = {
     },
     providers: [
         Credentials({
-                        credentials: {
-                            username: { label: "Username", type: "text", placeholder: "johndoe/janedoe" },
-                            password: { label: "Password", type: "password" },
-                            rememberMe: { label: "Remember Me", type: "checkbox", defaultValue: "false" },
-                        },
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        async authorize(credentials, _req) {
-                            const payload = {
-                                username: credentials.username,
-                                password: credentials.password,
-                                rememberMe: credentials.rememberMe,
-                            }
-                            
-                            const URL = loginAuthUrl()
-                            
-                            const response = await fetch(URL, {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify(payload),
-                            })
-                            
-                            const json = await response.json()
-                            if (!response.ok) throw JSON.stringify(json)
-                            
-                            const responsePayload = json as RestResponse
-                            const loginResponse = responsePayload.data as LoginResponse
-                            
-                            if (!responsePayload || !loginResponse) return null
-                            
-                            await createCookie(loginResponse.refreshToken, loginResponse.refreshTokenExpiresAt)
-                            
-                            return loginResponse
-                        },
-                    }),
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "johndoe/janedoe" },
+                password: { label: "Password", type: "password" },
+                rememberMe: { label: "Remember Me", type: "checkbox", defaultValue: "false" },
+            },
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            async authorize(credentials, _req) {
+                const payload = {
+                    username: credentials.username,
+                    password: credentials.password,
+                    rememberMe: credentials.rememberMe,
+                }
+
+                const URL = loginAuthUrl()
+
+                const response = await fetch(URL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(payload),
+                })
+
+                const json = await response.json()
+                if (!response.ok) throw JSON.stringify(json)
+
+                const responsePayload = json as RestResponse
+                const loginResponse = responsePayload.data as LoginResponse
+
+                if (!responsePayload || !loginResponse) return null
+
+                await createCookie(loginResponse.refreshToken, loginResponse.refreshTokenExpiresAt)
+
+                return loginResponse
+            },
+        }),
     ],
     callbacks: {
         async jwt({ token, user: context, account, trigger, session }) {
@@ -60,14 +60,14 @@ export const config = {
                 token.refreshToken = context.refreshToken
                 token.refreshTokenExpiresAt = context.refreshTokenExpiresAt
                 token.error = null
-                
+
                 return token
             }
-            
+
             if (trigger === "update") {
                 if (session) {
                     const refreshToken: { token: string; expiresAt: number } = await getTokenAndExpiration()
-                    
+
                     token.user = session.user
                     token.accessToken = session.accessToken
                     token.accessTokenExpiresAt = session.accessTokenExpiresAt
@@ -76,17 +76,17 @@ export const config = {
                     token.error = null
                 }
             }
-            
+
             if (
                 (token.accessTokenExpiresAt && Date.now() < new Date(token.accessTokenExpiresAt).getTime()) ||
                 token.error === "RefreshAccessTokenError"
             ) {
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 const { refreshToken, refreshTokenExpiresAt, ...rest } = token
-                
+
                 return rest
             }
-            
+
             return refreshAccessToken(token)
         },
         async session({ session, token }) {
@@ -94,7 +94,7 @@ export const config = {
             session.accessToken = token.accessToken
             session.accessTokenExpiresAt = token.accessTokenExpiresAt
             session.error = token.error
-            
+
             return session
         },
         async redirect({ url, baseUrl }) {
