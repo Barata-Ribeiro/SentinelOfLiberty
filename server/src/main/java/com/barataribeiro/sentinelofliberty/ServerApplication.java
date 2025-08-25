0875e2bd13ba8028030502247dd2657b34310d88
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-08-25 09:46:40 -0300

    feat(repository): add custom ApplicationSimpleJpaRepository and RepositorySpecificationExecutor for enhanced entity ID retrieval

--- server/src/main/java/com/barataribeiro/sentinelofliberty/ServerApplication.java
+++ server/src/main/java/com/barataribeiro/sentinelofliberty/ServerApplication.java
@@ -1,10 +1,15 @@
 package com.barataribeiro.sentinelofliberty;
 
+import com.barataribeiro.sentinelofliberty.repositories.specifications.ApplicationSimpleJpaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = ApplicationSimpleJpaRepository.class)
+@EnableTransactionManagement
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ServerApplication {
 
